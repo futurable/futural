15389d25b0915686ffe4abce8d659a234654b730
Jarmo Kortetjärvi <jarmo.kortetjarvi@futurable.fi>
2012-11-07 14:29:35 +0200

    Add necessary jquery for loan applications

---
+++ bank/js/BankLoanApplication.js
@@ -0,0 +1,80 @@
+$(document).ready(function(){
+	$(".instalment").hide();
+	$(".loanTerm").hide();
+	// Loanterm suffix
+	$( "#loanTermDiv").append( "<p id='repaymentIntervalText'>" + $("#repaymentInterval").val() + "</p>" );
+
+	// Toggling required fields
+	$("#loanType").change(function(){
+		// Show repayment
+		if($("#loanType").val() == "fixedRepayment"){
+			$(".repayment").fadeIn("slow");
+			if($(".instalment").is(":visible")){ $(".instalment").hide(); }
+			if($(".loanTerm").is(":visible")){ $(".loanTerm").hide(); }
+		}
+		// Show instalment
+		if($("#loanType").val() == "fixedInstalment"){
+			if($(".repayment").is(":visible")){ $(".repayment").hide(); }
+			$(".instalment").fadeIn("slow");
+			if($(".loanTerm").is(":visible")){ $(".loanTerm").hide(); }
+		}
+		// Show term
+		if($("#loanType").val() == "annuity"){
+			if($(".repayment").is(":visible")){ $(".repayment").hide(); }
+			if($(".instalment").is(":visible")){ $(".instalment").hide(); }
+			$(".loanTerm").fadeIn("slow");
+			
+			// Loanterm suffix
+			$( "#repaymentIntervalText" ).text( $("#repaymentInterval option:selected").text() );
+		}
+	});
+
+	$("#repaymentInterval").change(function(){
+		$( "#repaymentIntervalText" ).text( $("#repaymentInterval").val() );
+	});
+
+	/*// Loan term slider
+	$(function() {
+		$( "#loanTermSlider" ).slider({
+    		min: 5,
+    		max: 30, 0       
+    		 
+    		 
+    		  0
+    		value: 5,
+    		slide: function( event, ui ) {
+     			$( "#loanTerm" ).val( ui.value );
+     			$( "#loanTermRange" ).text( $("#repaymentInterval option:selected").text() + "s" );
+    		}
+		});
+ 		$( "#loanTerm" ).val( $( "#loanTermSlider" ).slider( "value" ) );
+ 		$( "#loanTermRange" ).text( $("#repaymentInterval option:selected").text() + "s" );
+	});
+	//*/
+
+	// Form validation
+	$("#loanApplicationForm").validate({
+		rules: {
+			loanAmount: {
+				required: true,
+				number: true
+			},
+			repayment: {
+				number: true
+			},
+			instalment: {
+				number: true
+			},
+			loanTerm: {
+				digit: true,
+				min: 5,
+				max: 30
+			}
+		}
+	});
+	
+	// Tooltips
+	$(function() {
+		$( document ).tooltip();
+	});
+});
