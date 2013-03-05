ead3691d8762aa43b2de25c63c48322815db137b
Jarmo Kortetjärvi <jarmo.kortetjarvi@futurable.fi>
2013-03-05 11:49:19 +0200

    Remove logging

--- backend/protected/config/main.php
+++ backend/protected/config/main.php
@@ -63,9 +63,9 @@ return array(
 					'levels'=>'error, warning',
 				),
 				// uncomment the following to show log messages on web pages
-				array(
+				/*array(
 					'class'=>'CWebLogRoute',
-				),
+				),*/
 			),
 		),
 	),
