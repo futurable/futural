f2d8ff80eedc89d702b9e9409a02c0adcd3d16d0
Jarmo Kortetjärvi <jarmo.kortetjarvi@futurable.fi>
2013-03-05 12:19:26 +0200

    Add light form field validation

--- backend/protected/views/tokenSettings/_form.php
+++ backend/protected/views/tokenSettings/_form.php
@@ -9,6 +9,12 @@
 <?php $form=$this->beginWidget('CActiveForm', array(
 	'id'=>'token-settings-form',
 	'enableAjaxValidation'=>false,
+	'enableClientValidation'=>true,
+	'clientOptions' => array(
+			'validateOnType'=>true,
+			'validateOnChange'=>true,
+			'validateOnSubmit'=>true,
+	),
 )); ?>
 
 	<p class="note">Fields with <span class="required">*</span> are required.</p>
@@ -16,37 +22,28 @@
 	<?php echo $form->errorSummary($model); ?>
 
 	<div class="row">
-		<?php echo $form->labelEx($model,'Description'); ?>
-		<?php echo $form->textField($model,'Description',array('size'=>32,'maxlength'=>32)); ?>
-		<?php echo $form->error($model,'Description'); ?>
+		<?php echo $form->labelEx($model,'Token_Customer_ID'); ?>
+    	<?php echo $form->dropDownList($model, 'Token_Customer_ID', CHtml::listData(TokenCustomer::model()->findAll(array('order'=>'Name')),'ID','Name'),
+                array('prompt'=>'- Select customer -'));?>	
+    	<?php echo $form->error($model,'Token_Customer_ID'); ?>
 	</div>
 
 	<div class="row">
 		<?php echo $form->labelEx($model,'CreateInitData'); ?>
-		<?php echo $form->textField($model,'CreateInitData'); ?>
+		<?php echo $form->checkBox($model, 'CreateInitData'); ?>
 		<?php echo $form->error($model,'CreateInitData'); ?>
 	</div>
 
 	<div class="row">
 		<?php echo $form->labelEx($model,'CreateDemoData'); ?>
-		<?php echo $form->textField($model,'CreateDemoData'); ?>
+		<?php echo $form->checkBox($model, 'CreateDemoData'); ?>
 		<?php echo $form->error($model,'CreateDemoData'); ?>
 	</div>
 
 	<div class="row">
 		<?php echo $form->labelEx($model,'CreatePurchasingOrders'); ?>
-		<?php echo $form->textField($model,'CreatePurchasingOrders'); ?>
+		<?php echo $form->checkBox($model, 'CreatePurchasingOrders'); ?>
 		<?php echo $form->error($model,'CreatePurchasingOrders'); ?>
-	</div>
-
-	<div class="row">
-		<?php echo $form->labelEx($model,'Token_Customer_ID'); ?>
-		<?php echo $form->textField($model,'Token_Customer_ID'); ?>
-		<?php echo $form->error($model,'Token_Customer_ID'); ?>
-	</div>
-
-	<div class="row buttons">
-		<?php echo CHtml::submitButton($model->isNewRecord ? 'Create' : 'Save'); ?>
 	</div>
 
 <?php $this->endWidget(); ?>
