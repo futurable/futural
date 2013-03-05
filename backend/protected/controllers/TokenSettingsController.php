31ece07b2de0841f716ac865ef4613d53b247b3e
Jarmo Kortetjärvi <jarmo.kortetjarvi@futurable.fi>
2013-03-05 12:28:38 +0200

    Modify access rules

--- backend/protected/controllers/TokenSettingsController.php
+++ backend/protected/controllers/TokenSettingsController.php
@@ -27,16 +27,16 @@ class TokenSettingsController extends Controller
 	public function accessRules()
 	{
 		return array(
-			array('allow',  // allow all users to perform 'index' and 'view' actions
-				'actions'=>array('index','view'),
+			array('allow',  // allow all users to perform no actions
+				'actions'=>array(''),
 				'users'=>array('*'),
 			),
-			array('allow', // allow authenticated user to perform 'create' and 'update' actions
-				'actions'=>array('create','update'),
+			array('allow', // allow authenticated user to perform 'index' and 'view' actions
+				'actions'=>array('index','view'),
 				'users'=>array('@'),
 			),
-			array('allow', // allow admin user to perform 'admin' and 'delete' actions
-				'actions'=>array('admin','delete'),
+			array('allow', // allow admin user to perform all actions
+				'actions'=>array('admin','delete', 'create','update'),
 				'users'=>array('admin'),
 			),
 			array('deny',  // deny all users
