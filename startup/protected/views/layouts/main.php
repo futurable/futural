87066a7a94bf9448ccffd6f9e7784cd8a6432d6f
Jarmo Kortetjärvi <jarmo.kortetjarvi@futurable.fi>
2013-03-08 09:32:16 +0200

    Update menu layout

--- startup/protected/views/layouts/main.php
+++ startup/protected/views/layouts/main.php
@@ -25,7 +25,17 @@
 	<div id="header">
 		<div id="logo"><?php echo CHtml::encode(Yii::app()->name); ?></div>
 	</div><!-- header -->
-
+        
+	<div id="mainmenu">
+		<?php /*
+			$this->widget('zii.widgets.CMenu',array(
+			'items'=>array(
+				array('label'=>'Home', 'url'=>array('/site/index')),
+				array('label'=>'Company', 'url'=>array('/company/index')),
+                            ),
+		)); */ ?>
+	</div><!-- mainmenu -->
+        
 	<?php echo $content; ?>
 
 	<div class="clear"></div>
