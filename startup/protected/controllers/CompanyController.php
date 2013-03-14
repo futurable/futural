4117aa000c754c86a560f2284fd13950081232ed
Jarmo Kortetjärvi <jarmo.kortetjarvi@futurable.fi>
2013-03-14 11:29:34 +0200

    Remove control rule for verify

--- startup/protected/controllers/CompanyController.php
+++ startup/protected/controllers/CompanyController.php
@@ -32,7 +32,7 @@ class CompanyController extends Controller
 				'users'=>array('*'),
 			),
 			array('allow', // allow authenticated user to perform 'create' and 'update' actions
-				'actions'=>array('create','update', 'verify'),
+				'actions'=>array('create','update'),
 				'users'=>array('@'),
 			),
 			array('allow', // allow admin user to perform 'admin' and 'delete' actions
