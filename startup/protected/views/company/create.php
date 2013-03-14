157b36be2f0102058dc94ded66d0300f2d947e91
Jarmo Kortetjärvi <jarmo.kortetjarvi@futurable.fi>
2013-03-14 12:31:49 +0200

    Add placeholder error text

--- startup/protected/views/company/create.php
+++ startup/protected/views/company/create.php
@@ -9,5 +9,6 @@
 
 if($company->form_step == 1) echo $this->renderPartial('_formStep1', array('token'=>$token));
 elseif($company->form_step == 2) echo $this->renderPartial('_formStep2', array('company'=>$company, 'industry'=>$industry));
+else echo "Error while deciding form step";
 
 ?>
