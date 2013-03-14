93a47a596c183a15febda5872a1d4caac817c5d9
Jarmo Kortetjärvi <jarmo.kortetjarvi@futurable.fi>
2013-03-14 13:31:50 +0200

    Add variables for render

--- startup/protected/views/company/create.php
+++ startup/protected/views/company/create.php
@@ -8,7 +8,19 @@
 <?php 
 
 if($company->form_step == 1) echo $this->renderPartial('_formStep1', array('token'=>$token));
-elseif($company->form_step == 2) echo $this->renderPartial('_formStep2', array('company'=>$company, 'industry'=>$industry));
+elseif($company->form_step == 2) echo $this->renderPartial('_formStep2', 
+        array(
+            'company'=>$company, 
+            'industry'=>$industry,
+            'costBenefitCalculation'=>$costBenefitCalculation,
+            'costBenefitItem_turnover'=>$costBenefitItem_turnover,
+            'costBenefitItem_salaries'=>$costBenefitItem_salaries,
+            'costBenefitItem_expenses'=>$costBenefitItem_expenses,
+            'costBenefitItem_loans'=>$costBenefitItem_loans,
+            'costBenefitItem_rents'=>$costBenefitItem_rents,
+            'costBenefitItem_communication'=>$costBenefitItem_communication,
+            'costBenefitItem_health'=>$costBenefitItem_health,
+        ));
 else echo "Error while deciding form step";
 
 ?>
