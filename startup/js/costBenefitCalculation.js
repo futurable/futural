37035029dd5a124b23457ee4ab789b46ca81e4fc
Jarmo Kortetjärvi <jarmo.kortetjarvi@futurable.fi>
2013-03-27 10:59:36 +0200

    Add turnover calculations

--- startup/js/costBenefitCalculation.js
+++ startup/js/costBenefitCalculation.js
@@ -1,3 +1,17 @@
 $(document).ready(function(){
+    // Turnover
+    $("#CostbenefitItem_turnover_value").keyup(function(){
+        var monthly = $("#CostbenefitItem_turnover_value").val();
+        var yearly = monthly * 12;
+        
+        $("#turnover").val(yearly);
+    });
+    $("#turnover").keyup(function(){
+       var yearly = $("#turnover").val();
+       var monthly = yearly / 12;
+       monthly = Math.round(monthly*100) / 100;
+       
+       $("#CostbenefitItem_turnover_value").val(monthly);
+    });
 
 });
