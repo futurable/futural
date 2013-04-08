84d1b9abd18c8b96ed470f343b4f69ec199b3c59
Jarmo Kortetjärvi <jarmo.kortetjarvi@futurable.fi>
2013-04-08 12:16:21 +0300

    Remove obsolete fields

--- startup/protected/views/company/view.php
+++ startup/protected/views/company/view.php
@@ -1,29 +1,8 @@
 <?php
 /* @var $this CompanyController */
 /* @var $model Company */
-
-$this->breadcrumbs=array(
-	'Companies'=>array('index'),
-	$model->name,
-);
-
-$this->menu=array(
-	array('label'=>'List Company', 'url'=>array('index')),
-	array('label'=>'Create Company', 'url'=>array('create')),
-	array('label'=>'Update Company', 'url'=>array('update', 'id'=>$model->id)),
-	array('label'=>'Delete Company', 'url'=>'#', 'linkOptions'=>array('submit'=>array('delete','id'=>$model->id),'confirm'=>'Are you sure you want to delete this item?')),
-	array('label'=>'Manage Company', 'url'=>array('admin')),
-);
 ?>
 
-<h1>View Company #<?php echo $model->id; ?></h1>
+<h1><?php echo $model->name; ?></h1>
 
-<?php $this->widget('zii.widgets.CDetailView', array(
-	'data'=>$model,
-	'attributes'=>array(
-		'id',
-		'name',
-		'token_key_id',
-		'industry_id',
-	),
-)); ?>
+<?php ?>
