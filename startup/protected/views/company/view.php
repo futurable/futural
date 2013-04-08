3f3c09fe710d4d4931b6fe2174966042a3da43a7
Jarmo Kortetjärvi <jarmo.kortetjarvi@futurable.fi>
2013-04-08 13:15:57 +0300

    Add placeholder fields

--- startup/protected/views/company/view.php
+++ startup/protected/views/company/view.php
@@ -1,7 +1,9 @@
 <?php
 /* @var $this CompanyController */
-/* @var $model Company */
-/* @var $model Industry */
+/* @var $company    Company */
+/* @var $industry   Industry */
+/* @var $CBC        Cost-benefit calculation */
+/* @var $CBC_items  Cost-benefit calculation items */
 ?>
 
 <h1><?php echo $company->name; ?></h1>
@@ -10,9 +12,37 @@
     <tr>
         <th>Industry</th>
         <th>Description</th>
+        <th>Medium turnover</th>
+        <th>Minimum wage rate</th>
     </tr>
     <tr>
-        <td><?php echo $industry->name; ?></td>
+        <td><?php echo ucfirst($industry->name); ?></td>
         <td><?php echo $industry->description; ?></td>
+        <td><?php echo $industrySetups[0]->turnover." &euro;";?></td>
+        <td><?php echo $industrySetups[0]->minimum_wage_rate." &euro;";?></td>
     </tr>
+</table>
+
+<h2>Cost-benefit calculation</h2>
+<table>
+    <tr>
+        <th>Type</th>
+        <th>Monthly cost</th>
+        <th>Description</th>
+    </tr>
+<?php
+    foreach($CBC_items as $item){
+        $type = $item->costbenefitItemType;
+        $typeName = ucfirst($type->name);
+        
+        $tableRow = "
+            <tr>
+                <td>$typeName</td>
+                <td>$item->value &euro;</td>
+                <td>$type->description</td>
+            </tr>";
+        
+        echo $tableRow;
+    }
+?>
 </table>
