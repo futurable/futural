03b843613bba5231e4cc659415903fe4521b0ef2
Jarmo Kortetjärvi <jarmo.kortetjarvi@futurable.fi>
2013-04-15 17:41:19 +0300

    Add favicon

--- startup/protected/views/layouts/main.php
+++ startup/protected/views/layouts/main.php
@@ -16,6 +16,7 @@
 	<link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->request->baseUrl; ?>/css/main.css" />
 	<link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->request->baseUrl; ?>/css/form.css" />
 	<link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->request->baseUrl; ?>/css/futural.css" />
+        <link rel="shortcut icon" href="<?php echo Yii::app()->request->baseUrl; ?>/css/img/favicon.ico" />
 
 	<title><?php echo CHtml::encode($this->pageTitle); ?></title>
 </head>
