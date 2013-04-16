f3535f207f04b509ce708e7ee5ed0d5151ea2298
Jarmo Kortetjärvi <jarmo.kortetjarvi@futurable.fi>
2013-04-16 16:23:20 +0300

    Add model for TokenCustomer

---
+++ startup/protected/models/TokenCustomer.php
@@ -0,0 +1,117 @@
+<?php
+
+/**
+ * This is the model class for table "token_customer".
+ *
+ * The followings are the available columns in table 'token_customer':
+ * @property integer $id
+ * @property string $tag
+ * @property string $name
+ * @property string $street
+ * @property string $city
+ * @property string $phone
+ * @property string $email
+ *
+ * The followings are the available model relations:
+ * @property TokenKey[] $tokenKeys
+ * @property TokenSetup[] $tokenSetups
+ */
+class TokenCustomer extends CActiveRecord
+{
+	/**
+	 * @return string the associated database table name
+	 */
+	public function tableName()
+	{
+		return 'token_customer';
+	}
+
+	/**
+	 * @return array validation rules for model attributes.
+	 */
+	public function rules()
+	{
+		// NOTE: you should only define rules for those attributes that
+		// will receive user inputs.
+		return array(
+			array('tag, name', 'required'),
+			array('tag', 'length', 'max'=>32),
+			array('name, street, city, email', 'length', 'max'=>256),
+			array('phone', 'length', 'max'=>128),
+			// The following rule is used by search().
+			// @todo Please remove those attributes that should not be searched.
+			array('id, tag, name, street, city, phone, email', 'safe', 'on'=>'search'),
+		);
+	}
+
+	/**
+	 * @return array relational rules.
+	 */
+	public function relations()
+	{
+		// NOTE: you may need to adjust the relation name and the related
+		// class name for the relations automatically generated below.
+		return array(
+			'tokenKeys' => array(self::HAS_MANY, 'TokenKey', 'token_customer_id'),
+			'tokenSetups' => array(self::HAS_MANY, 'TokenSetup', 'token_customer_id'),
+		);
+	}
+
+	/**
+	 * @return array customized attribute labels (name=>label)
+	 */
+	public function attributeLabels()
+	{
+		return array(
+			'id' => 'ID',
+			'tag' => 'Tag',
+			'name' => 'Name',
+			'street' => 'Street',
+			'city' => 'City',
+			'phone' => 'Phone',
+			'email' => 'Email',
+		);
+	}
+
+	/**
+	 * Retrieves a list of models based on the current search/filter conditions.
+	 *
+	 * Typical usecase:
+	 * - Initialize the model fields with values from filter form.
+	 * - Execute this method to get CActiveDataProvider instance which will filter
+	 * models according to data in model fields.
+	 * - Pass data provider to CGridView, CListView or any similar widget.
+	 *
+	 * @return CActiveDataProvider the data provider that can return the models
+	 * based on the search/filter conditions.
+	 */
+	public function search()
+	{
+		// @todo Please modify the following code to remove attributes that should not be searched.
+
+		$criteria=new CDbCriteria;
+
+		$criteria->compare('id',$this->id);
+		$criteria->compare('tag',$this->tag,true);
+		$criteria->compare('name',$this->name,true);
+		$criteria->compare('street',$this->street,true);
+		$criteria->compare('city',$this->city,true);
+		$criteria->compare('phone',$this->phone,true);
+		$criteria->compare('email',$this->email,true);
+
+		return new CActiveDataProvider($this, array(
+			'criteria'=>$criteria,
+		));
+	}
+
+	/**
+	 * Returns the static model of the specified AR class.
+	 * Please note that you should have this exact method in all your CActiveRecord descendants!
+	 * @param string $className active record class name.
+	 * @return TokenCustomer the static model class
+	 */
+	public static function model($className=__CLASS__)
+	{
+		return parent::model($className);
+	}
+}
