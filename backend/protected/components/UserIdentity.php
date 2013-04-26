952a9ca20fe6114ec99b4db57f5d4588f43fdcd0
Jarmo Kortetjärvi <jarmo.kortetjarvi@futurable.fi>
2013-04-26 15:48:51 +0300

    Add database user authentication

--- backend/protected/components/UserIdentity.php
+++ backend/protected/components/UserIdentity.php
@@ -7,27 +7,25 @@
  */
 class UserIdentity extends CUserIdentity
 {
-	/**
-	 * Authenticates a user.
-	 * The example implementation makes sure if the username and password
-	 * are both 'demo'.
-	 * In practical applications, this should be changed to authenticate
-	 * against some persistent user identity storage (e.g. database).
-	 * @return boolean whether authentication succeeds.
-	 */
-	public function authenticate()
-	{
-		$users=array(
-			// username => password
-			'demo'=>'demo',
-			'admin'=>'admin',
-		);
-		if(!isset($users[$this->username]))
-			$this->errorCode=self::ERROR_USERNAME_INVALID;
-		elseif($users[$this->username]!==$this->password)
-			$this->errorCode=self::ERROR_PASSWORD_INVALID;
-		else
-			$this->errorCode=self::ERROR_NONE;
-		return !$this->errorCode;
-	}
+    private $id;
+ 
+    public function authenticate()
+    {
+        $record=User::model()->findByAttributes(array('email'=>$this->username));
+        if($record===null)
+            $this->errorCode=self::ERROR_USERNAME_INVALID;
+        else if($record->password!==hash('sha512', $this->password))
+            $this->errorCode=self::ERROR_PASSWORD_INVALID;
+        else
+        {
+            $this->id=$record->id;
+            $this->setState('role', $record->role);            
+            $this->errorCode=self::ERROR_NONE;
+        }
+        return !$this->errorCode;
+    }
+ 
+    public function getId(){
+        return $this->id;
+    }
 }
