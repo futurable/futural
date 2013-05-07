9cbd1261ead79fb1bbb3d1564a3e8dc60c19e694
Jarmo Kortetjärvi <jarmo.kortetjarvi@futurable.fi>
2013-05-07 10:50:17 +0300

    Modify view link from id to name

--- backend/protected/views/tokenCustomer/_view.php
+++ backend/protected/views/tokenCustomer/_view.php
@@ -6,7 +6,7 @@
 <div class="view">
 
 	<b><?php echo CHtml::encode($data->getAttributeLabel('id')); ?>:</b>
-	<?php echo CHtml::link(CHtml::encode($data->id), array('view', 'id'=>$data->id)); ?>
+	<?php echo CHtml::encode($data->id); ?>
 	<br />
 
 	<b><?php echo CHtml::encode($data->getAttributeLabel('tag')); ?>:</b>
@@ -14,7 +14,7 @@
 	<br />
 
 	<b><?php echo CHtml::encode($data->getAttributeLabel('name')); ?>:</b>
-	<?php echo CHtml::encode($data->name); ?>
+	<?php echo CHtml::link(CHtml::encode($data->name), array('view', 'id'=>$data->id)); ?>
 	<br />
 
 	<b><?php echo CHtml::encode($data->getAttributeLabel('street')); ?>:</b>
