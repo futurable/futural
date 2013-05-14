a7d28a7042e8d9bc1b9d2c352f93d30973c509ee
jarmo <jarmo@futurable-jarmo>
2013-05-14 14:34:16 +0300

    Add loop to go through suppliers

--- backend/protected/controllers/order/CreateAction.php
+++ backend/protected/controllers/order/CreateAction.php
@@ -1,6 +1,7 @@
 <?php
 class CreateAction extends CAction
 {
+    private $runLog;
     /**
      * Lists all models.
      */
@@ -9,12 +10,14 @@ class CreateAction extends CAction
         $controller=$this->getController();
     
         // 1. Check if orders are already made for this week
+        $runlog[] = date('h:i:s')."Orders not checked!";
         
         // 2. Get supplier firms
         $supplierCompanies = Yii::app()->db->createCommand()
                 ->select('tag, name')
                 ->from('company')
                 ->queryAll();
+        $runlog[] = "Got ".count($supplierCompanies)." suppliers.";
         
         // Get supplier names in array
         $supplierNames = null;
@@ -23,12 +26,18 @@ class CreateAction extends CAction
         }
         $supplierNames = substr($supplierNames, 0, -1);
         
+        // Get all suppliers that exist in OpenERP
         $supplierOpenerp = Yii::app()->dbopenerp->createCommand()
             ->select('datname')
             ->from('pg_database')
             ->where('datistemplate = false AND datname = ANY(\'{'.$supplierNames.'}\')')
             ->queryAll();
-
+        // Existing suppliers to array
+        $existingSuppliers = array();
+        foreach($supplierOpenerp as $supplier){
+            $existingSuppliers[$supplier['datname']] = $supplier['datname'];
+        }
+        
         $supplierData=new CActiveDataProvider('Company');
 
         // 3. Get customer firms
@@ -36,7 +45,14 @@ class CreateAction extends CAction
             ->select('name')
             ->from('res_company')
             ->queryAll();
-          
+        
+        // 4. Run through each supplier
+        foreach($supplierCompanies as $supplier){
+            if(!array_key_exists($supplier['tag'], $existingSuppliers)) continue;
+            
+            //echo "<p>$supplier[name]</p>";
+        }
+        
         $customerData=new CActiveDataProvider('ResCompany');
         
         $model=new Order;
@@ -53,8 +69,9 @@ class CreateAction extends CAction
 
         $controller->render('create',array(
                 'model'=>$model,
+                'runLog'=>$runlog,
                 'customerData'=>$customerData,
-                'supplierData'=>$supplierData
+                'supplierData'=>$supplierData,
         ));
     }
 }
