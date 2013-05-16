6e43ae573c72fbe9066356abd897ab822fc3c9cd
jarmo <jarmo@futurable-jarmo>
2013-05-16 10:30:56 +0300

    Add tooltips

--- startup/protected/views/company/_formStep2.php
+++ startup/protected/views/company/_formStep2.php
@@ -50,26 +50,42 @@ Yii::app()->clientScript->registerScriptFile(Yii::app()->baseUrl . '/js/costBene
 	<div class="row">
                 <?php echo $form->hiddenField($token, 'token_key', array('value'=>$token->token_key)); ?>
 		<?php echo $form->labelEx($company,'name'); ?>
-		<?php echo $form->textField($company,'name',array('size'=>30,'maxlength'=>256)); ?>
+		<?php echo $form->textField($company,'name', array(
+                                                                'size'=>30,
+                                                                'maxlength'=>256, 
+                                                                'rel'=>'tooltip', 
+                                                                'title'=>'The company name.')); ?>
 		<?php echo $form->error($company,'name'); ?>
 	</div>
         
 	<div class="row">
 		<?php echo $form->labelEx($company,'email'); ?>
-		<?php echo $form->textField($company,'email',array('size'=>30,'maxlength'=>256)); ?>
+		<?php echo $form->textField($company,'email',   array(
+                                                                'size'=>30,
+                                                                'maxlength'=>256,
+                                                                'rel'=>'tooltip',
+                                                                'title'=>'System admin email. You will receive the account information here.')); ?>
 		<?php echo $form->error($company,'email'); ?>
 	</div>
 
 	<div class="row">
 		<?php echo $form->labelEx($company,'industry_id'); ?>
-                <?php echo $form->dropDownList($company, 'industry_id', CHtml::listData(Industry::model()->findAll(array('order'=>'Name')),'id','name'),array('prompt'=>'- Select industry -'));?>
+                <?php echo $form->dropDownList($company, 'industry_id', CHtml::listData(Industry::model()->findAll(array('order'=>'Name')),'id','name'),
+                                                            array(
+                                                                'prompt'=>'- Select industry -',
+                                                                'rel'=>'tooltip',
+                                                                'title'=>'Select the main industry for your company. It will be used to give you guideline values for the cost-benefit calculation.'));?>
 		<?php echo $form->error($company,'industry_id'); ?>
                 <span id="Company_industry_description"></span>
 	</div>
         
         <div class="row">
                 <?php echo CHtml::label("Employees", "employees");?>
-		<?php echo CHtml::dropDownList('Company_employees', 'Company_employees', array_merge( range(1,9),range(10,100,10)) ); ?>
+		<?php echo CHtml::dropDownList('Company_employees', 'Company_employees', array_merge( range(1,9),range(10,100,10)), 
+                                                            array(
+                                                                'rel'=>'tooltip',
+                                                                'title'=>'Employee amount. This is used to give guidelines for the salary calculation.'
+                                                            )); ?>
 	</div>
         
         <div class="row">
@@ -83,43 +99,64 @@ Yii::app()->clientScript->registerScriptFile(Yii::app()->baseUrl . '/js/costBene
                 </tr>
                 <tr>
                     <td><?php echo $form->labelEx($costBenefitItem_turnover,'Turnover'); ?></td>
-                    <td><?php echo $form->textField($costBenefitItem_turnover,'[turnover]value'); ?> &euro;</td>
+                    <td><?php echo $form->textField($costBenefitItem_turnover,'[turnover]value', array(
+                        'rel'=>'tooltip',
+                        'title'=>'Total monthly turnover.'
+                     )); ?> &euro;</td>
                     <td><?php echo CHtml::textField('[turnover]yearly'); ?> &euro;</td>
                     <td><?php echo $form->error($costBenefitItem_turnover,'[turnover]value'); ?></td>
                 </tr>
                 <tr>
                     <td><?php echo $form->labelEx($costBenefitItem_expenses,'Expenses'); ?></td>
-                    <td><?php echo $form->textField($costBenefitItem_expenses,'[expenses]value'); ?> &euro;</td>
+                    <td><?php echo $form->textField($costBenefitItem_expenses,'[expenses]value', array(
+                        'rel'=>'tooltip',
+                        'title'=>'Montly expenses. Ex. materials, products etc.'
+                     )); ?> &euro;</td>
                     <td><?php echo CHtml::textField('[expenses]yearly'); ?> &euro;</td>
                     <td><?php echo $form->error($costBenefitItem_expenses,'[expenses]value'); ?></td>
                 </tr>
                 <tr>
                     <td><?php echo $form->labelEx($costBenefitItem_salaries,'Salaries'); ?></td>
-                    <td><?php echo $form->textField($costBenefitItem_salaries,'[salaries]value'); ?> &euro;</td>
+                    <td><?php echo $form->textField($costBenefitItem_salaries,'[salaries]value', array(
+                        'rel'=>'tooltip',
+                        'title'=>'Total employee salaries without side expenses.'
+                     )); ?> &euro;</td>
                     <td><?php echo CHtml::textField('[salaries]yearly'); ?> &euro;</td>
                     <td><?php echo $form->error($costBenefitItem_salaries,'[salaries]value'); ?></td>
                 </tr>
                 <tr>
                     <td><?php echo $form->labelEx($costBenefitItem_loans,'Loans'); ?></td>
-                    <td><?php echo $form->textField($costBenefitItem_loans,'[loans]value'); ?> &euro;</td>
+                    <td><?php echo $form->textField($costBenefitItem_loans,'[loans]value', array(
+                        'rel'=>'tooltip',
+                        'title'=>'Loan costs. Includes service costs, interest and instalments.'
+                     )); ?> &euro;</td>
                     <td><?php echo CHtml::textField('[loans]yearly'); ?> &euro;</td>
                     <td><?php echo $form->error($costBenefitItem_loans,'[loans]value'); ?></td>
                 </tr>
                 <tr>
                     <td><?php echo $form->labelEx($costBenefitItem_rents,'Rents'); ?></td>
-                    <td><?php echo $form->textField($costBenefitItem_rents,'[rents]value'); ?> &euro;</td>
+                    <td><?php echo $form->textField($costBenefitItem_rents,'[rents]value', array(
+                        'rel'=>'tooltip',
+                        'title'=>'Total rents. Includes office rent and possible warehouse rents'
+                     )); ?> &euro;</td>
                     <td><?php echo CHtml::textField('[rents]yearly'); ?> &euro;</td>
                     <td><?php echo $form->error($costBenefitItem_rents,'[rents]value'); ?></td>
                 </tr>
                 <tr>
                     <td><?php echo $form->labelEx($costBenefitItem_communication,'Communication'); ?></td>
-                    <td><?php echo $form->textField($costBenefitItem_communication,'[communication]value'); ?> &euro;</td>
+                    <td><?php echo $form->textField($costBenefitItem_communication,'[communication]value', array(
+                        'rel'=>'tooltip',
+                        'title'=>'Company communications. Ex. phones, data connections. Does not include marketing costs.'
+                     )); ?> &euro;</td>
                     <td><?php echo CHtml::textField('[communication]yearly'); ?> &euro;</td>
                     <td><?php echo $form->error($costBenefitItem_communication,'[communication]value'); ?></td>
                 </tr>
                 <tr>
                     <td><?php echo $form->labelEx($costBenefitItem_health,'Health'); ?></td>
-                    <td><?php echo $form->textField($costBenefitItem_health,'[health]value'); ?> &euro;</td>
+                    <td><?php echo $form->textField($costBenefitItem_health,'[health]value', array(
+                        'rel'=>'tooltip',
+                        'title'=>'Mandatory or voluntary employee health plan costs.'
+                     )); ?> &euro;</td>
                     <td><?php echo CHtml::textField('[health]yearly'); ?> &euro;</td>
                     <td><?php echo $form->error($costBenefitItem_health,'[health]value'); ?></td>
                 </tr>
