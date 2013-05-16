8cf83a88700860146ebe6253ed15f24e69a840ff
jarmo <jarmo@futurable-jarmo>
2013-05-16 13:31:19 +0300

    Add email

--- startup/protected/controllers/company/CreateAction.php
+++ startup/protected/controllers/company/CreateAction.php
@@ -133,7 +133,6 @@ class CreateAction extends CAction
                     $allSuccessful = $companySuccess AND $CBCSuccess;
                     if($allSuccessful){
                         $transaction->commit();
-                        
                         // Generate password
                         
                         // Create OpenERP database
@@ -142,6 +141,7 @@ class CreateAction extends CAction
                         $shellCmd = escapeshellcmd($cmd);
                         $scriptFile = Yii::app()->basePath."/commands/shell/createOpenERPCompany.sh";
                         $output = exec("sh ".$scriptFile.$shellCmd);
+                        $email = $company->email;
                         
                         // Create business ID
                         $businessID = "123"; // TODO: do business ID
@@ -162,7 +162,7 @@ class CreateAction extends CAction
                         
                         Have fun!";
                         
-                        mail('jarmo@futurable.fi', "Futurality account", $message);
+                        mail($email, "Futurality account", $message);
                         
                         // Redirect to view
                         $controller->redirect(array('view','id'=>$company->id));
