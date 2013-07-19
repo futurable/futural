c768d5f56a1e76afe0aa1e5fe36cadeaf165099f
Jarmo Kortetjärvi <jarmo.kortetjarvi@futurable.fi>
2013-07-19 11:06:24 +0300

    Remove debug

--- startup/js/costBenefitCalculation.js
+++ startup/js/costBenefitCalculation.js
@@ -128,7 +128,7 @@ $(document).ready(function(){
         
         // Calculate loan sum. 3x all expenses + one months expenses
         loanSum = (expenses+salaries+rents+communication)*3 + expenses;
-        var interest = 3.3 / 100 / 12; alert(interest);
+        var interest = 3.3 / 100 / 12;
         payment = loanSum * ( interest / (1 - Math.pow((1+interest), -36)));
         
         loans = Math.round(payment);
