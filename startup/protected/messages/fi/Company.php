0e8dd812469831740e0af1d424747fe048ea03fb
Jarmo Kortetjärvi <jarmo.kortetjarvi@futurable.fi>
2013-07-23 11:49:39 +0300

    Add translations for the mail

--- startup/protected/messages/fi/Company.php
+++ startup/protected/messages/fi/Company.php
@@ -9,7 +9,7 @@
  * Each array element represents the translation (value) of a message (key).
  * If the value is empty, the message is considered as not translated.
  * Messages that no longer need translation will have their translations
- * enclosed between a pair of '@@' marks.
+ * enclosed between a pair of '' marks.
  *
  * Message string can be used with plural forms format. Check i18n section
  * of the guide for details.
@@ -17,8 +17,22 @@
  * NOTE, this file must be saved in UTF-8 encoding.
  */
 return array (
-  'Create' => 'Luo',
+  'BankAccount' => 'Pankkitunnukset',
+  'FuturalityLearningEnvironment' => 'Futurality-oppimisympäristöön',
+  'HaveFun' => 'Pidä hauskaa!',
+  'LoginFrom' => 'Kirjautuminen',
+  'OpenERPAccount' => 'OpenERP-tunnukset',
+  'Password' => 'Salasana',
+  'UserId' => 'Käyttäjätunnus',
+  'WelcomeToFuturality' => 'Tervetuloa Futuralityyn!',
+  'YouHaveCreatedACompanyInThe' => 'Olet luonut yrityksen',
+  'YourBusinessIdIs' => 'Y-tunnuksesi on',
+  'YourCompanyNameIs' => 'Yrityksesi nimi on',
+  'YourCompanyTagIs' => 'Yrityksesi tunnus on',
+  'computer-assembly' => 'Tietokoneiden kokoonpano',
+  'computer-retail' => 'Tietokoneiden jälleenmyynti',
   'Communication' => 'Yhteydet',
+  'Create' => 'Luo',
   'CreateCompany' => 'Luo yritys',
   'EachTokenKeyCanBeUsedOnlyOnce' => 'Jokaisen  perustamisavaimen voi käyttää vain kerran',
   'Email' => 'Sähköposti',
@@ -57,6 +71,4 @@ return array (
   'Turnover' => 'Liikevaihto',
   'Verify' => 'Tarkista',
   'Yearly' => 'Vuodessa',
-  'computer-assembly' => 'Tietokoneiden kokoonpano',
-  'computer-retail' => 'Tietokoneiden jälleenmyynti',
 );
