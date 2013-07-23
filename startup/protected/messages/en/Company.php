df84f4e668d66b44f7801fa20a3c14e655f1236c
Jarmo Kortetjärvi <jarmo.kortetjarvi@futurable.fi>
2013-07-23 14:10:42 +0300

    Add translations

--- startup/protected/messages/en/Company.php
+++ startup/protected/messages/en/Company.php
@@ -9,7 +9,7 @@
  * Each array element represents the translation (value) of a message (key).
  * If the value is empty, the message is considered as not translated.
  * Messages that no longer need translation will have their translations
- * enclosed between a pair of '@@' marks.
+ * enclosed between a pair of '' marks.
  *
  * Message string can be used with plural forms format. Check i18n section
  * of the guide for details.
@@ -17,15 +17,20 @@
  * NOTE, this file must be saved in UTF-8 encoding.
  */
 return array (
-  'BusinessId' => 'Business ID',
-  'CostBenefitCalculation' => 'Cost-benefit calculation',
-  'Description' => 'Kuvaus',
+  'TheERPSystem' => 'The ERP system',
+  'TheBank' => 'The bank',
+  'TheLearningEnvironment' => 'The Futurality learning environment',
+  'WhatIsNext' => 'What\'s next?',
+  'OtherExpenses' => 'Other expenses',
   'computer-assembly' => 'Computer assembly',
   'computer-retail' => 'Computer retail',
   'BankAccount' => 'Bank account',
+  'BusinessId' => 'Business ID',
   'Communication' => 'Communication',
+  'CostBenefitCalculation' => 'Cost-benefit calculation',
   'Create' => 'Create',
   'CreateCompany' => 'Create a company',
+  'Description' => 'Kuvaus',
   'EachTokenKeyCanBeUsedOnlyOnce' => 'Each token key can be used only once',
   'Email' => 'Email',
   'Employees' => 'Employees',
