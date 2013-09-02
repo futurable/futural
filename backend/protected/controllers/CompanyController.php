64e2f5307400cdbc7eff6bb5d45f08869799b8ad
Jarmo Kortetjärvi <jarmo.kortetjarvi@futurable.fi>
2013-09-02 16:18:53 +0300

    Add list action

--- backend/protected/controllers/CompanyController.php
+++ backend/protected/controllers/CompanyController.php
@@ -32,7 +32,7 @@ class CompanyController extends Controller
 				'users'=>array('*'),
 			),
 			array('allow', // allow authenticated user to perform 'create' and 'update' actions
-				'actions'=>array('create','update'),
+				'actions'=>array('create','update', 'list'),
 				'users'=>array('@'),
 			),
 			array('allow', // allow admin user to perform 'admin' and 'delete' actions
@@ -45,6 +45,13 @@ class CompanyController extends Controller
 		);
 	}
 
+    public function actions()
+    {
+        return array(
+            'list'=>'application.controllers.company.ListAction',
+        );
+    }
+    
 	/**
 	 * Displays a particular model.
 	 * @param integer $id the ID of the model to be displayed
