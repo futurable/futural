dbd0db96354fe9a29c236513c8a57917d293652d
Jarmo Kortetjärvi <jarmo.kortetjarvi@futurable.fi>
2013-09-03 17:43:48 +0300

    Add a placeholder view

--- backend/protected/views/company/list.php
+++ backend/protected/views/company/list.php
@@ -1,1 +1,28 @@
 <h1>List companies</h1>
+
+<div class="well">
+<?php
+    $gridDataProvider = new CArrayDataProvider($suppliers);
+
+    $gridColumns = array(
+        array('name'=>'name', 'header'=>'Name'),
+        array('name'=>'businessId', 'header'=>'Business ID'),
+        array('name'=>'email', 'header'=>'Email'),
+        array(
+            'htmlOptions' => array('nowrap'=>'nowrap'),
+            'class'=>'bootstrap.widgets.TbButtonColumn',
+            'viewButtonUrl'=>null,
+            'updateButtonUrl'=>null,
+            'deleteButtonUrl'=>null,
+        )
+    );
+
+    $this->widget('bootstrap.widgets.TbGridView', array(
+        'type'=>'striped',
+        'dataProvider'=>$gridDataProvider,
+        'template'=>"{items}",
+        'columns'=>$gridColumns,
+    ));
+
+?>
+</div>
