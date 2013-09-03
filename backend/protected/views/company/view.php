c87f680ec4c17d90dc103a16e32cfbf2173e3656
Jarmo Kortetjärvi <jarmo.kortetjarvi@futurable.fi>
2013-09-03 17:56:22 +0300

    Add view skeleton

--- backend/protected/views/company/view.php
+++ backend/protected/views/company/view.php
@@ -1,30 +1,7 @@
-<?php
-/* @var $this CompanyController */
-/* @var $model Company */
-
-$this->breadcrumbs=array(
-	'Companies'=>array('index'),
-	$model->name,
-);
-
-$this->menu=array(
-	array('label'=>'List Company', 'url'=>array('index')),
-	array('label'=>'Create Company', 'url'=>array('create')),
-	array('label'=>'Update Company', 'url'=>array('update', 'id'=>$model->id)),
-	array('label'=>'Delete Company', 'url'=>'#', 'linkOptions'=>array('submit'=>array('delete','id'=>$model->id),'confirm'=>'Are you sure you want to delete this item?')),
-	array('label'=>'Manage Company', 'url'=>array('admin')),
-);
+<?php 
+    echo "
+    <h1>
+        {$model->name} <br/>
+        {$model->business_id} <br/>
+    </h1>";
 ?>
-
-<h1>View Company #<?php echo $model->id; ?></h1>
-
-<?php $this->widget('zii.widgets.CDetailView', array(
-	'data'=>$model,
-	'attributes'=>array(
-		'id',
-		'name',
-		'tag',
-		'token_key_id',
-		'industry_id',
-	),
-)); ?>
