4351c611033629c203435fd6f4a2a7462b377232
Jarmo Kortetjärvi <jarmo.kortetjarvi@futurable.fi>
2013-09-03 18:40:28 +0300

    Add company info and bank accounts

--- backend/protected/views/company/view.php
+++ backend/protected/views/company/view.php
@@ -1,7 +1,39 @@
 <?php 
-    echo "
-    <h1>
-        {$model->name} <br/>
-        {$model->business_id} <br/>
-    </h1>";
+    // Company info
+    echo "<h1>{$company->name}</h1>";
+    
+    $this->widget('bootstrap.widgets.TbDetailView', array(
+        'data'=>$company,
+        'attributes'=>array(
+            array('name'=>'tag', 'label'=>'Tag'),
+            array('name'=>'business_id', 'label'=>'Business ID'),
+            array('name'=>'email', 'label'=>'Email'),
+            array('name'=>'create_time', 'label'=>'Created'),
+        ),
+    ));
+    
+    // Bank accounts info
+    echo "<h2>Bank Accounts</h2>";
+    
+    $gridDataProvider = new CArrayDataProvider($bankAccounts);
+
+    $gridColumns = array(
+        array('name'=>'iban', 'header'=>'Name'),
+        array('name'=>'name', 'header'=>'Account name'),
+        array('name'=>'create_date', 'header'=>'Create date'),
+        array(
+            'htmlOptions' => array('nowrap'=>'nowrap'),
+            'class'=>'bootstrap.widgets.TbButtonColumn',
+            'viewButtonUrl'=>'Yii::app()->createUrl("/bankAccount/view", array("id" => $data["id"]))',
+            'updateButtonUrl'=>null,
+            'deleteButtonUrl'=>null,
+        )
+    );
+
+    $this->widget('bootstrap.widgets.TbGridView', array(
+        'type'=>'striped',
+        'dataProvider'=>$gridDataProvider,
+        'template'=>"{items}",
+        'columns'=>$gridColumns,
+    ));
 ?>
