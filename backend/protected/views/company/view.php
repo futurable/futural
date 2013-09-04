586d210177cc135e4bc0585fa91aa5c9dd78cc7e
jarmo <jarmo@futurable-jarmo>
2013-09-04 12:57:25 +0300

    Modify view structure

--- backend/protected/views/company/view.php
+++ backend/protected/views/company/view.php
@@ -1,39 +1,9 @@
 <?php 
-    // Company info
-    echo "<h1>{$company->name}</h1>";
-    
-    $this->widget('bootstrap.widgets.TbDetailView', array(
-        'data'=>$company,
-        'attributes'=>array(
-            array('name'=>'tag', 'label'=>'Tag'),
-            array('name'=>'business_id', 'label'=>'Business ID'),
-            array('name'=>'email', 'label'=>'Email'),
-            array('name'=>'create_time', 'label'=>'Created'),
-        ),
+    $this->renderPartial('_viewCompanyInfo',array(
+	'company'=>$company
     ));
     
-    // Bank accounts info
-    echo "<h2>Bank Accounts</h2>";
-    
-    $gridDataProvider = new CArrayDataProvider($bankAccounts);
-
-    $gridColumns = array(
-        array('name'=>'iban', 'header'=>'Name'),
-        array('name'=>'name', 'header'=>'Account name'),
-        array('name'=>'status', 'header'=>'Status'),
-        array(
-            'htmlOptions' => array('nowrap'=>'nowrap'),
-            'class'=>'bootstrap.widgets.TbButtonColumn',
-            'viewButtonUrl'=>'Yii::app()->createUrl("/bankAccount/view", array("id" => $data["id"]))',
-            'updateButtonUrl'=>null,
-            'deleteButtonUrl'=>null,
-        )
-    );
-
-    $this->widget('bootstrap.widgets.TbGridView', array(
-        'type'=>'striped',
-        'dataProvider'=>$gridDataProvider,
-        'template'=>"{items}",
-        'columns'=>$gridColumns,
+   $this->renderPartial('_viewBankAccounts',array(
+	'bankAccounts'=>$bankAccounts
     ));
 ?>
