4063b96e9eaa43962b509daf5339ec012ffdef5a
jarmo <jarmo@futurable-jarmo>
2013-09-04 12:57:35 +0300

    Add a subview

---
+++ backend/protected/views/company/_viewBankAccounts.php
@@ -0,0 +1,26 @@
+<?php
+// Bank accounts info
+    echo "<h2>Bank Accounts</h2>";
+    
+    $gridDataProvider = new CArrayDataProvider($bankAccounts);
+
+    $gridColumns = array(
+        array('name'=>'iban', 'header'=>'Name'),
+        array('name'=>'name', 'header'=>'Account name'),
+        array('name'=>'status', 'header'=>'Status'),
+        array(
+            'htmlOptions' => array('nowrap'=>'nowrap'),
+            'class'=>'bootstrap.widgets.TbButtonColumn',
+            'viewButtonUrl'=>'Yii::app()->createUrl("/bankAccount/view", array("id" => $data["id"]))',
+            'updateButtonUrl'=>null,
+            'deleteButtonUrl'=>null,
+        )
+    );
+
+    $this->widget('bootstrap.widgets.TbGridView', array(
+        'type'=>'striped',
+        'dataProvider'=>$gridDataProvider,
+        'template'=>"{items}",
+        'columns'=>$gridColumns,
+    ));
+ ?>
