587846f7e8bfa5353a0f38542f7885cbae86f5e8
jarmo <jarmo@futurable-jarmo>
2013-09-04 09:22:03 +0300

    Modify indentation

--- backend/protected/config/main.php
+++ backend/protected/config/main.php
@@ -14,84 +14,82 @@ return array(
 
 	// autoloading model and component classes
 	'import'=>array(
-        'application.models.*',
-        'application.components.*',
+            'application.models.*',
+            'application.components.*',
 	),
 
 	'modules'=>array(
-        // uncomment the following to enable the Gii tool
-        'gii'=>array(
-            'class'=>'system.gii.GiiModule',
-            'password'=>'futural',
-            // If removed, Gii defaults to localhost only. Edit carefully to taste.
-            'ipFilters'=>array('127.0.0.1','::1'),
-        ),
-        'generatorPaths' => array(
-            'bootstrap.gii'
-        ),
+            // uncomment the following to enable the Gii tool
+            'gii'=>array(
+                'class'=>'system.gii.GiiModule',
+                'password'=>'futural',
+                // If removed, Gii defaults to localhost only. Edit carefully to taste.
+                'ipFilters'=>array('127.0.0.1','::1'),
+            ),
+            'generatorPaths' => array(
+                'bootstrap.gii'
+            ),
 	),
 
 	// application components
 	'components'=>array(
-		'user'=>array(
-            // enable cookie-based authentication
-            'allowAutoLogin'=>true,
-		),
-		// uncomment the following to enable URLs in path-format
-		'urlManager'=>array(
+            'user'=>array(
+                // enable cookie-based authentication
+                'allowAutoLogin'=>true,
+            ),
+            // uncomment the following to enable URLs in path-format
+            'urlManager'=>array(
             'urlFormat'=>'path',
             'rules'=>array(
                 '<controller:\w+>/<id:\d+>'=>'<controller>/view',
                 '<controller:\w+>/<action:\w+>/<id:\d+>'=>'<controller>/<action>',
                 '<controller:\w+>/<action:\w+>'=>'<controller>/<action>',
-            ),
-		),
-		// uncomment the following to use a MySQL database
-		'db'=>array(
-            'connectionString' => 'mysql:host=futurality.fi;dbname=futural_core',
-            'emulatePrepare' => true,
-            'username' => 'futural',
-            'password' => 'futural',
-            'charset' => 'utf8',
-		),
-		// uncomment the following to use a MySQL database
-		'dbopenerp'=>array(
-            'connectionString' => 'pgsql:host=erp.futurality.fi;dbname=futu_sopuli',
-            'emulatePrepare' => true,
-            'username' => 'openerp',
-            'password' => 'futural',
-            'charset' => 'utf8',
-            'class' => 'CDbConnection' 
-		),
-        'bootstrap' => array(
-            'class' => 'ext.bootstrap.components.Bootstrap',
-            'responsiveCss' => true,
-        ),
-		'errorHandler'=>array(
-            // use 'site/error' action to display errors
-            'errorAction'=>'site/error',
-		),
-		'log'=>array(
-            'class'=>'CLogRouter',
-            'routes'=>array(
-                array(
-                    'class'=>'CFileLogRoute',
-                    'levels'=>'error, warning',
                 ),
-                // uncomment the following to show log messages on web pages
-                /*
-                array(
+            ),
+            'db'=>array(
+                'connectionString' => 'mysql:host=futurality.fi;dbname=futural_core',
+                'emulatePrepare' => true,
+                'username' => 'futural',
+                'password' => 'futural',
+                'charset' => 'utf8',
+            ),
+            'dbopenerp'=>array(
+                'connectionString' => 'pgsql:host=erp.futurality.fi;dbname=futu_sopuli',
+                'emulatePrepare' => true,
+                'username' => 'openerp',
+            'password' => 'futural',
+                'charset' => 'utf8',
+                'class' => 'CDbConnection' 
+            ),
+            'bootstrap' => array(
+                'class' => 'ext.bootstrap.components.Bootstrap',
+                'responsiveCss' => true,
+            ),
+            'errorHandler'=>array(
+                // use 'site/error' action to display errors
+                'errorAction'=>'site/error',
+            ),
+            'log'=>array(
+                'class'=>'CLogRouter',
+                'routes'=>array(
+                    array(
+                        'class'=>'CFileLogRoute',
+                        'levels'=>'error, warning',
+                    ),
+                    // uncomment the following to show log messages on web pages
+                    /*
+                    array(
                         'class'=>'CWebLogRoute',
+                    ),
+                    //*/
                 ),
-                //*/
             ),
-		),
 	),
 
 	// application-level parameters that can be accessed
 	// using Yii::app()->params['paramName']
 	'params'=>array(
-        // this is used in contact page
-        'adminEmail'=>'helpdesk@futurable.fi',
+            // this is used in contact page
+            'adminEmail'=>'helpdesk@futurable.fi',
 	),
 );
