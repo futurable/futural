2d50bc4e12745a4041e39016fbd723ce56d15d02
Jarmo Kortetjärvi <jarmo.kortetjarvi@futurable.fi>
2013-09-05 10:20:10 +0300

    Move ViewAction to an external file

---
+++ backend/protected/controllers/company/ViewAction.php
@@ -0,0 +1,32 @@
+<?php
+class ViewAction extends CAction
+{
+    public function run()
+    {
+        $controller=$this->getController();
+        
+        $company = $controller->loadModel($id);
+        $bankUser = BankUser::model()->findByAttributes(array('username'=>$company->tag));
+
+        $bankAccounts = BankAccount::model()->findAll(
+            array(
+                'condition'=>'bank_user_id=:bank_user_id', 
+                'params'=>array('bank_user_id'=>$bankUser->id),
+            )
+        );
+
+        Yii::app()->dbopenerp->setActive(false);
+        Yii::app()->dbopenerp->connectionString = "pgsql:host=erp.futurality.fi;dbname={$company->tag}";
+        Yii::app()->dbopenerp->setActive(true);
+        $OEHrEmployees = HrEmployee::model()->findAll();
+        $OESaleOrders = SaleOrder::model()->findAll(array('order'=>'create_date DESC'));
+
+        $controller->render('view',array(
+            'company'=>$company,
+            'bankAccounts'=>$bankAccounts,
+            'OEHrEmployees'=>$OEHrEmployees,
+            'OESaleOrders'=>$OESaleOrders,
+        ));
+    }
+}
+?>
