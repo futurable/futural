df5bf8f4b650f168008dc64b176ef4b36e3b869d
Jarmo Kortetjärvi <jarmo.kortetjarvi@futurable.fi>
2013-09-06 13:16:06 +0300

    Add user access control

--- backend/protected/controllers/company/ListAction.php
+++ backend/protected/controllers/company/ListAction.php
@@ -3,7 +3,9 @@ class ListAction extends CAction
 {
     public function run()
     {
+        $this->allowUser(MANAGER);
         $controller=$this->getController();
+        
         $suppliers = $controller->getSuppliers();
         
         $controller->render('list',array(
