da1ced139a8563422a0e30278013f5ef6eacd573
Jarmo Kortetjärvi <jarmo.kortetjarvi@futurable.fi>
2013-09-06 11:26:21 +0300

    Add a relation

--- backend/protected/models/User.php
+++ backend/protected/models/User.php
@@ -47,6 +47,7 @@ class User extends ActiveRecord
 		// NOTE: you may need to adjust the relation name and the related
 		// class name for the relations automatically generated below.
 		return array(
+            'token_customer'=>array(self::BELONGS_TO, 'token_customer', 'token_customer_id'),
 		);
 	}
 
