2421d0f01681ee0a014b5c68ed283dbe9be7420b
Jarmo Kortetjärvi <jarmo.kortetjarvi@futurable.fi>
2013-09-06 12:28:45 +0300

    Modify view

--- backend/protected/views/site/index.php
+++ backend/protected/views/site/index.php
@@ -8,7 +8,7 @@ $this->pageTitle=Yii::app()->name;
 
     <h1>Welcome to <?php echo CHtml::encode(Yii::app()->name) ;?>!</h1>
 
-    <p>You are logged in as <?php echo Yii::app()->user->name; ?></p>
+    <p><?php echo "You are logged in as  ".Yii::app()->user->name; ?></p>
 <?php
 }
 else $this->redirect(array('/site/login'));
