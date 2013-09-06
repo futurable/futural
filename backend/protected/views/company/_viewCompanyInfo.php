091d23ddff83f2c770af7fdd1068d1f958b70426
Jarmo Kortetjärvi <jarmo.kortetjarvi@futurable.fi>
2013-09-06 15:07:46 +0300

    Modify headers

--- backend/protected/views/company/_viewCompanyInfo.php
+++ backend/protected/views/company/_viewCompanyInfo.php
@@ -1,10 +1,11 @@
 <?php
     // Company info
-    echo "<h1>{$company->name}</h1>";
+    echo "<h2>Company Info</h2>";
     
     $this->widget('bootstrap.widgets.TbDetailView', array(
         'data'=>$company,
         'attributes'=>array(
+            array('name'=>'name', 'label'=>'Name'),
             array('name'=>'tag', 'label'=>'Tag'),
             array('name'=>'business_id', 'label'=>'Business ID'),
             array('name'=>'email', 'label'=>'Email'),
