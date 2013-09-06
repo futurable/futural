66d18103cc5e4dfbfb589bfe6883229c5e51ec56
Jarmo Kortetjärvi <jarmo.kortetjarvi@futurable.fi>
2013-09-06 12:09:42 +0300

    Add webuser component

---
+++ backend/protected/components/WebUser.php
@@ -0,0 +1,31 @@
+<?php
+class WebUser extends CWebUser {
+ 
+    // Store model to not repeat query.
+    private $_model;
+ 
+    // Return first name.
+    // access it by Yii::app()->user->first_name
+    function getEmail(){
+        $user = $this->loadUser(Yii::app()->user->email);
+        return $user->email;
+    }
+ 
+  // This is a function that checks the field 'role'
+  // in the User model to be equal to 1, that means it's admin
+  // access it by Yii::app()->user->isAdmin()
+    function isAdmin(){
+        $user = $this->loadUser(Yii::app()->user->id);
+        return intval($user->role) == 1;
+    }
+ 
+  // Load user model.
+    protected function loadUser($id=null){
+        if($this->_model===null){
+            if($id!==null)
+                $this->_model=User::model()->findByPk($id);
+        }
+        return $this->_model;
+    }
+}
+?>
