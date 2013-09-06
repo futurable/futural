6080d2cf7656573d14b32cb2ab34416482015069
Jarmo Kortetjärvi <jarmo.kortetjarvi@futurable.fi>
2013-09-06 14:59:02 +0300

    Add a submenu

--- backend/protected/views/company/view.php
+++ backend/protected/views/company/view.php
@@ -12,4 +12,14 @@
         'OESaleOrders'=>$OESaleOrders,
         'OEPurchaseOrders'=>$OEPurchaseOrders,
     ));
+    
+    $this->menu=array(
+        array('label'=>'Company Info', 'url'=>array("/company/view", 'id' => $company->id)),
+        array('label'=>'Bank Accounts', 'url'=>array("/company/view", 'id' => $company->id, 'action'=>'bankAccounts')),
+        array('label'=>'Employees', 'url'=>array("/company/view", 'id' => $company->id, 'action'=>'employees')),
+        array('label'=>'Sale orders', 'url'=>array("/company/view", 'id' => $company->id, 'action'=>'saleOrders')),
+        array('label'=>'Purchase orders', 'url'=>array("/company/view", 'id' => $company->id, 'action'=>'purchaseOrders')),
+    );
+
+
 ?>
