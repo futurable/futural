fa00607a8da833cf461246ffafc56aa251a2a2fe
Jarmo Kortetjärvi <jarmo.kortetjarvi@futurable.fi>
2013-09-06 15:11:25 +0300

    Add view for sale orders

---
+++ backend/protected/views/company/_viewOpenErpSaleOrders.php
@@ -0,0 +1,31 @@
+<?php
+    echo "<h2>Sale orders</h2>";
+    
+    $gridDataProvider = new CArrayDataProvider($OESaleOrders, array(           
+        'pagination'=>array(
+            'pageSize' => 5,
+         ),
+    ));
+
+    $gridColumns = array(
+        array('name'=>'create_date', 'header'=>'Created'),
+        array('name'=>'amount_total', 'header'=>'Order total'),
+        array('name'=>'state', 'header'=>'Order state'),
+        array(
+            'htmlOptions' => array('nowrap'=>'nowrap'),
+            'class'=>'bootstrap.widgets.TbButtonColumn',
+            'viewButtonUrl'=>null,
+            'buttons'=>array(
+                'update'=>array('visible'=>'false',),
+                'delete'=>array('visible'=>'false',),
+            ),
+        )
+    );
+
+    $this->widget('bootstrap.widgets.TbGridView', array(
+        'type'=>'striped',
+        'dataProvider'=>$gridDataProvider,
+        'template'=>"{items}{pager}",
+        'columns'=>$gridColumns,
+    ));
+?>
