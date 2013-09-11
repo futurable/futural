8dbd1942723bb21f23e76cb5a320e43110bd7b2f
Jarmo Kortetjärvi <jarmo.kortetjarvi@futurable.fi>
2013-09-11 16:39:15 +0300

    Add realized values to the table

--- backend/protected/views/company/_viewCostBenefitCalculation.php
+++ backend/protected/views/company/_viewCostBenefitCalculation.php
@@ -2,29 +2,34 @@
     // Company info
     echo "<h2>Cost-benefit calculation</h2>";
     
+    $week = date('W');
+    
     echo "<table>";
         echo "<tr>";
-            echo "<th>Expense</th>";
+            echo "<th></th>";
             echo "<th>Planned</th>";
             echo "<th>Realized</th>";
         echo "</tr>";
         
-        echo getTableRow($costBenefitCalculationItems['turnover']);
-        echo getTableRow($costBenefitCalculationItems['expenses']);
-        echo getTableRow($costBenefitCalculationItems['salaries']);
-        echo getTableRow($costBenefitCalculationItems['sideExpenses']);
-        echo getTableRow($costBenefitCalculationItems['loans']);
-        echo getTableRow($costBenefitCalculationItems['rents']);
-        echo getTableRow($costBenefitCalculationItems['communication']);
-        echo getTableRow($costBenefitCalculationItems['health']);
-        echo getTableRow($costBenefitCalculationItems['otherExpenses']);
+        echo getTableRow($costBenefitCalculationItems['turnover'], '300000', $realizedItems);
+        echo getTableRow($costBenefitCalculationItems['expenses'], '400000', $realizedItems);
+        echo getTableRow($costBenefitCalculationItems['salaries'], '50000', $realizedItems);
+        echo getTableRow($costBenefitCalculationItems['sideExpenses'], '50000', $realizedItems);
+        echo getTableRow($costBenefitCalculationItems['loans'], null, $realizedItems);
+        echo getTableRow($costBenefitCalculationItems['rents'], '701010', $realizedItems);
+        echo getTableRow($costBenefitCalculationItems['communication'], '703000', $realizedItems);
+        echo getTableRow($costBenefitCalculationItems['health'], '70000', $realizedItems);
+        echo getTableRow($costBenefitCalculationItems['otherExpenses'], '707010', $realizedItems);
     echo "</table>";
-    
-    function getTableRow($CBCItem){
+
+    function getTableRow($CBCItem, $account, $realizedItems){
+        $realized = isset($realizedItems[$account]) ? $realizedItems[$account] : "0.00";
+        
         $row = "
         <tr>
             <th>".ucfirst($CBCItem->costbenefitItemType->name)."</th>
             <td>{$CBCItem->value} &euro;</td>
+            <td>{$realized} &euro;</td>
         </tr>";
         
         return $row;
