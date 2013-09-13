bfbb86beee49458c17834a2fe06429e4191dbf0b
jarmo <jarmo@futurable-jarmo>
2013-09-13 15:28:53 +0300

    Add translations

--- backend/protected/views/company/_viewOpenErpSaleOrders.php
+++ backend/protected/views/company/_viewOpenErpSaleOrders.php
@@ -1,5 +1,5 @@
 <?php
-    echo "<h2>Sale orders</h2>";
+    echo "<h2>".Yii::t('Company', 'SaleOrders')."</h2>";
     
     $gridDataProvider = new CArrayDataProvider($OESaleOrders, array(           
         'pagination'=>array(
@@ -8,9 +8,9 @@
     ));
 
     $gridColumns = array(
-        array('name'=>'create_date', 'header'=>'Created'),
-        array('name'=>'amount_total', 'header'=>'Order total'),
-        array('name'=>'state', 'header'=>'Order state'),
+        array('name'=>'create_date', 'header'=>Yii::t('Company', 'Created')),
+        array('name'=>'amount_total', 'header'=>Yii::t('Company', 'OrderTotal')),
+        array('name'=>'state', 'header'=>Yii::t('Company', 'OrderState')),
         array(
             'htmlOptions' => array('nowrap'=>'nowrap'),
             'class'=>'bootstrap.widgets.TbButtonColumn',
