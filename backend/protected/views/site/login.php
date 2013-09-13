2c6c816200091a7164db596a7e7f8b683c536208
jarmo <jarmo@futurable-jarmo>
2013-09-13 14:48:02 +0300

    Modify labels

--- backend/protected/views/site/login.php
+++ backend/protected/views/site/login.php
@@ -10,13 +10,13 @@
 )); ?>
 
 	<div class="row">
-		<?php echo $form->labelEx($model,'username'); ?>
+		<?php echo $form->label($model,'username'); ?>
 		<?php echo $form->textField($model,'username'); ?>
 		<?php echo $form->error($model,'username'); ?>
 	</div>
 
 	<div class="row">
-		<?php echo $form->labelEx($model,'password'); ?>
+		<?php echo $form->label($model,'password'); ?>
 		<?php echo $form->passwordField($model,'password'); ?>
 		<?php echo $form->error($model,'password'); ?>
 	</div>
