ad77f1960b58e200fe3077b330482c1ce26a0889
jarmo <jarmo@futurable-jarmo>
2013-09-13 14:51:25 +0300

    Add translations

--- backend/protected/messages/en/Company.php
+++ backend/protected/messages/en/Company.php
@@ -9,7 +9,7 @@
  * Each array element represents the translation (value) of a message (key).
  * If the value is empty, the message is considered as not translated.
  * Messages that no longer need translation will have their translations
- * enclosed between a pair of '@@' marks.
+ * enclosed between a pair of '' marks.
  *
  * Message string can be used with plural forms format. Check i18n section
  * of the guide for details.
@@ -17,15 +17,15 @@
  * NOTE, this file must be saved in UTF-8 encoding.
  */
 return array (
+  'Communication' => 'Data communications',
+  'Expenses' => 'Expenses',
+  'FacilityExpenses' => 'Facility expenses',
+  'Health' => 'Health',
+  'Loans' => 'Loans',
+  'OtherExpenses' => 'Other expensest',
+  'SalariesAndSideExpenses' => 'Salaries and side expenses',
+  'Turnover' => 'Turnover',
   'CostBenefitCalculation' => 'Cost-benefit calculation',
   'Planned' => 'Planned',
   'Realized' => 'Realized',
-  'Turnover' => 'Turnover',
-  'Expenses' => 'Expenses',
-  'SalariesAndSideExpenses' => 'Salaries and side expenses',
-  'Loans' => 'Loans',
-  'FacilityExpenses' => 'Facility expenses',
-  'Communication' => 'Data communications',
-  'Health' => 'Health',
-  'OtherExpenses' => 'Other expensest',
 );
