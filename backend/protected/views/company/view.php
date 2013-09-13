8ef98b83034002226bf4d398eee94276612e3cce
jarmo <jarmo@futurable-jarmo>
2013-09-13 15:23:55 +0300

    Add translations

--- backend/protected/views/company/view.php
+++ backend/protected/views/company/view.php
@@ -1,11 +1,11 @@
 <?php
     $this->menu=array(
-        array('label'=>Yii::t('Menu', 'CompanyInfo'), 'url'=>array("/company/view", 'id' => $company->id, 'action'=>'info')),
-        array('label'=>Yii::t('Menu', 'CostBenefitCalculation'), 'url'=>array("/company/view", 'id' => $company->id, 'action'=>'costBenefitCalculation')),
-        array('label'=>Yii::t('Menu', 'BankAccounts'), 'url'=>array("/company/view", 'id' => $company->id, 'action'=>'bankAccounts')),
-        array('label'=>Yii::t('Menu', 'Employees'), 'url'=>array("/company/view", 'id' => $company->id, 'action'=>'employees')),
-        array('label'=>Yii::t('Menu', 'SaleOrders'), 'url'=>array("/company/view", 'id' => $company->id, 'action'=>'saleOrders')),
-        array('label'=>Yii::t('Menu', 'PurchaseOrders'), 'url'=>array("/company/view", 'id' => $company->id, 'action'=>'purchaseOrders')),
+        array('label'=>Yii::t('Company', 'CompanyInfo'), 'url'=>array("/company/view", 'id' => $company->id, 'action'=>'info')),
+        array('label'=>Yii::t('Company', 'CostBenefitCalculation'), 'url'=>array("/company/view", 'id' => $company->id, 'action'=>'costBenefitCalculation')),
+        array('label'=>Yii::t('Company', 'BankAccounts'), 'url'=>array("/company/view", 'id' => $company->id, 'action'=>'bankAccounts')),
+        array('label'=>Yii::t('Company', 'Employees'), 'url'=>array("/company/view", 'id' => $company->id, 'action'=>'employees')),
+        array('label'=>Yii::t('Company', 'SaleOrders'), 'url'=>array("/company/view", 'id' => $company->id, 'action'=>'saleOrders')),
+        array('label'=>Yii::t('Company', 'PurchaseOrders'), 'url'=>array("/company/view", 'id' => $company->id, 'action'=>'purchaseOrders')),
     );
 
     echo "<h1>{$company->name}</h1>";
