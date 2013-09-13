acdbf8b9948ac6dcfd3b26e14d39f08aa107ae8a
jarmo <jarmo@futurable-jarmo>
2013-09-13 15:32:00 +0300

    Add translations

--- backend/protected/views/company/_viewOpenErpPurchaseOrders.php
+++ backend/protected/views/company/_viewOpenErpPurchaseOrders.php
@@ -8,9 +8,9 @@
     ));
 
     $gridColumns = array(
-        array('name'=>'create_date'),
-        array('name'=>'amount_total'),
-        array('name'=>'state'),
+        array('name'=>'create_date', 'header' => Yii::t('Company', 'CreateDate')),
+        array('name'=>'amount_total', 'header' => Yii::t('Company', 'TotalAmount')),
+        array('name'=>'state', 'header' => Yii::t('Company', 'State')),
         array(
             'htmlOptions' => array('nowrap'=>'nowrap'),
             'class'=>'bootstrap.widgets.TbButtonColumn',
