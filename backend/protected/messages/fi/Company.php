022bcaf1362d8ccf9d7261ccdaf203a7f1c06dbb
jarmo <jarmo@futurable-jarmo>
2013-09-13 14:51:27 +0300

    Add translations

--- backend/protected/messages/fi/Company.php
+++ backend/protected/messages/fi/Company.php
@@ -9,7 +9,7 @@
  * Each array element represents the translation (value) of a message (key).
  * If the value is empty, the message is considered as not translated.
  * Messages that no longer need translation will have their translations
- * enclosed between a pair of '@@' marks.
+ * enclosed between a pair of '' marks.
  *
  * Message string can be used with plural forms format. Check i18n section
  * of the guide for details.
@@ -17,15 +17,15 @@
  * NOTE, this file must be saved in UTF-8 encoding.
  */
 return array (
+  'Communication' => 'Tietoliikenneyhteydet',
+  'Expenses' => 'Materiaalikulut',
+  'FacilityExpenses' => 'Toimitilakulut',
+  'Health' => 'Terveyskulut',
+  'Loans' => 'Lainat',
+  'OtherExpenses' => 'Muut kulut',
+  'SalariesAndSideExpenses' => 'Palkat ja sivukulut',
+  'Turnover' => 'Liikevaihto',
   'CostBenefitCalculation' => 'Kannattavuuslaskelma',
   'Planned' => 'Suunniteltu',
   'Realized' => 'Toteutunut',
-  'Turnover' => 'Liikevaihto',
-  'Expenses' => 'Materiaalikulut',
-  'SalariesAndSideExpenses' => 'Palkat ja sivukulut',
-  'Loans' => 'Lainat',
-  'FacilityExpenses' => 'Toimitilakulut',
-  'Communication' => 'Tietoliikenneyhteydet',
-  'Health' => 'Terveyskulut',
-  'OtherExpenses' => 'Muut kulut',
 );
