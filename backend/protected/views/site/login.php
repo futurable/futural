c733515968bae589ecb67a850404ad207aa8ae61
jarmo <jarmo@futurable-jarmo>
2013-09-13 14:52:42 +0300

    Remove "remember me"-button

--- backend/protected/views/site/login.php
+++ backend/protected/views/site/login.php
@@ -21,12 +21,6 @@
 		<?php echo $form->error($model,'password'); ?>
 	</div>
 
-	<div class="row rememberMe">
-		<?php echo $form->checkBox($model,'rememberMe'); ?>
-		<?php echo $form->label($model,'rememberMe'); ?>
-		<?php echo $form->error($model,'rememberMe'); ?>
-	</div>
-
 	<div class="row buttons">
 		<?php echo CHtml::submitButton('Login'); ?>
 	</div>
