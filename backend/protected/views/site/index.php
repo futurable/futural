4ac2731c06efdb12e9b9b7bf8540e39849932fbe
jarmo <jarmo@futurable-jarmo>
2013-09-13 15:47:43 +0300

    Modify formatting

--- backend/protected/views/site/index.php
+++ backend/protected/views/site/index.php
@@ -8,7 +8,8 @@ $this->pageTitle=Yii::app()->name;
 
     <h1><?php echo CHtml::encode(Yii::app()->name) ;?></h1>
 
-    <p><?php echo Yii::t('Index', 'YouAreLoggedInAs')." ".Yii::app()->user->tokenCustomer->name; ?></p>
+    <p><?php echo Yii::t('Index', 'YouAreLoggedInAs')."<br/><strong>".
+            Yii::app()->user->tokenCustomer->name; ?></strong></p>
 <?php
 }
 else $this->redirect(array('/site/login'));
