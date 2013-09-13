d611612289c3ca1d361f8cbf74f03f29f5151e80
jarmo <jarmo@futurable-jarmo>
2013-09-13 13:48:56 +0300

    Add multi-account support

--- backend/protected/views/company/_viewCostBenefitCalculation.php
+++ backend/protected/views/company/_viewCostBenefitCalculation.php
@@ -11,23 +11,42 @@
             echo "<th>Realized</th>";
         echo "</tr>";
         
-        echo getTableRow($costBenefitCalculationItems['turnover'], '300000', $realizedItems);
-        echo getTableRow($costBenefitCalculationItems['expenses'], '400000', $realizedItems);
-        echo getTableRow($costBenefitCalculationItems['salaries'], '50000', $realizedItems);
-        echo getTableRow($costBenefitCalculationItems['sideExpenses'], '50000', $realizedItems);
-        echo getTableRow($costBenefitCalculationItems['loans'], null, $realizedItems);
-        echo getTableRow($costBenefitCalculationItems['rents'], '701010', $realizedItems);
-        echo getTableRow($costBenefitCalculationItems['communication'], '703000', $realizedItems);
-        echo getTableRow($costBenefitCalculationItems['health'], '70000', $realizedItems);
-        echo getTableRow($costBenefitCalculationItems['otherExpenses'], '707010', $realizedItems);
+        echo getTableRow($costBenefitCalculationItems['turnover'], $realizedItems, array('300000'));
+        echo getTableRow($costBenefitCalculationItems['expenses'], $realizedItems, array('400000'));
+        echo getTableRow($costBenefitCalculationItems['salaries'], $realizedItems, array('500000'), 'SalariesAndSideExpenses');
+        echo getTableRow($costBenefitCalculationItems['loans'], $realizedItems);
+        echo getTableRow($costBenefitCalculationItems['rents'], $realizedItems, array('701010', '701080'), 'FacilityExpenses');
+        echo getTableRow($costBenefitCalculationItems['communication'], $realizedItems, array('703000'));
+        echo getTableRow($costBenefitCalculationItems['health'], $realizedItems, array('70000'));
+        echo getTableRow($costBenefitCalculationItems['otherExpenses'], $realizedItems, array('707010'));
     echo "</table>";
 
-    function getTableRow($CBCItem, $account, $realizedItems){
-        $realized = isset($realizedItems[$account]) ? $realizedItems[$account] : "0.00";
+    /**
+     * Function for getting printed rows
+     * 
+     * @param array $CBCItem // Cost-benefit calculation items
+     * @param array $realizedItems // Realized values
+     * @param array $account // Accounts to calculate
+     * @param type $label // Lavel for the row
+     * @return string
+     */
+    function getTableRow($CBCItem, $realizedItems, $account = false, $label = false){
+        $realized = 0;
+        if(is_array($account)){
+            foreach($account as $value){
+                $realized += isset($realizedItems[$value]) ? $realizedItems[$value] : 0;
+            }  
+        }
+        // Remove the negative operator
+        if($CBCItem->costbenefitItemType->name != 'turnover') $realized *= -1;
+        // Add 2 decimals
+        $realized = number_format($realized, 2, ',', ' ');
+        
+        $header = !empty($label) ? $label : ucfirst($CBCItem->costbenefitItemType->name);
         
         $row = "
         <tr>
-            <th>".ucfirst($CBCItem->costbenefitItemType->name)."</th>
+            <th>{$header}</th>
             <td>{$CBCItem->value} &euro;</td>
             <td>{$realized} &euro;</td>
         </tr>";
