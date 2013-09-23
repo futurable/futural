f77126e1960b98c2c29eb94a8447f0a490dc90a2
jarmo <jarmo@futurable-jarmo>
2013-09-23 15:47:51 +0300

    Add fetching suppliers

--- backend/protected/commands/CreateOrdersCommand.php
+++ backend/protected/commands/CreateOrdersCommand.php
@@ -1,9 +1,14 @@
 <?php
 class CreateOrdersCommand extends CConsoleCommand
-{
-    public function run($args)
-    {
+{ 
+    public function run(){
         echo( date('Y-m-d H:i:s').": CreateOrders run started.\n" );
+        
+        $suppliers = GetSuppliers::run();
+        
+        foreach($suppliers as $supplier){
+            echo "$supplier->name \n";
+        }
         
         echo( date('Y-m-d H:i:s').": CreateOrders run ended.\n\n" );
     }
