bfa5e5b1e0c81f75822bf3c6ab6a51673d95189b
Jarmo Kortetjärvi <jarmo.kortetjarvi@futurable.fi>
2013-09-24 14:47:03 +0300

    Add order executor file

---
+++ backend/protected/commands/ExecuteOrdersCommand.php
@@ -0,0 +1,16 @@
+<?php
+class ExcecuteOrdersCommand extends CConsoleCommand
+{
+    public function run($args)
+    {   
+        # 1. Get all customers
+        $businessCenterDb = Yii::app()->params['businessCenterDb'];
+        
+        // Change OpenERP-database
+        Yii::app()->dbopenerp->setActive(false);
+        Yii::app()->dbopenerp->connectionString = "pgsql:host=erp.futurality.fi;dbname={$businessCenterDb}";
+        Yii::app()->dbopenerp->setActive(true);
+        
+        $customers = ResCompany::model()->findAll();
+    }
+}
