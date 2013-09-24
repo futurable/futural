e5f57780c51e9be3b812bb2d76b89de8176f82ca
Jarmo Kortetjärvi <jarmo.kortetjarvi@futurable.fi>
2013-09-24 15:38:52 +0300

    Add order setups and order amount calculation

--- backend/protected/commands/CreateOrdersCommand.php
+++ backend/protected/commands/CreateOrdersCommand.php
@@ -19,19 +19,31 @@ class CreateOrdersCommand extends CConsoleCommand
         $suppliers = Suppliers::getAll();
         
         # 3. Get the order setups
-       $orderSetups = OrderSetup::model()->findAll();
-        
+        $orderSetups = GetOrderSetupArray::run();
+ 
         # 4. Run through each firm
         foreach($suppliers as $supplier){
             echo( "Using company '{$supplier->name}'\n" );
-            
+
+            // Get the customer tag
+            $customerTag = $supplier->tokenKey->tokenCustomer->tag;
+            // Get the customer setup
+            $orderSetup = isset($orderSetups[ $customerTag ]) ? $orderSetups[ $customerTag ] : $orderSetups[ 'default' ];
+
             // Use the first cost-benefit calculation
             $costBenefitCalculation = $supplier->costbenefitCalculations[0];
             // Get the turnover
             $criteria = new CDbCriteria;
             $criteria->condition = 'costbenefit_calculation_id=:id AND costbenefit_item_type_id=1';
             $criteria->params = array(':id'=>$costBenefitCalculation->id);
-            $turnover = CostbenefitItem::model()->find($criteria);
+            $turnover = CostbenefitItem::model()->find($criteria)->value;
+
+            // Decide how many orders will be made in week
+
+            $orderAmount = 0;
+            $orderAmount += $orderSetup[ 'product' ]->amount;
+            $orderAmount += $orderSetup[ 'group' ]->amount;
+            $orderAmount += $orderSetup[ 'random' ]->amount;
 
         }
 
