920e9cf73ff192feb7279adb1b949cdfb80b333e
Jarmo Kortetjärvi <jarmo.kortetjarvi@futurable.fi>
2013-09-24 13:05:40 +0300

    Add customer fetching

--- backend/protected/commands/CreateOrdersCommand.php
+++ backend/protected/commands/CreateOrdersCommand.php
@@ -3,11 +3,19 @@ class CreateOrdersCommand extends CConsoleCommand
 { 
     public function run(){
         echo( date('Y-m-d H:i:s').": CreateOrders run started.\n" );
-        
+            
         $suppliers = Suppliers::getAll();
+        $businessCenterDb = Yii::app()->params['businessCenterDb'];
+        
+        // Change OpenERP-database
+        Yii::app()->dbopenerp->setActive(false);
+        Yii::app()->dbopenerp->connectionString = "pgsql:host=erp.futurality.fi;dbname={$businessCenterDb}";
+        Yii::app()->dbopenerp->setActive(true);
+        
+        $customers = ResCompany::model()->findAll();
         
         foreach($suppliers as $supplier){
-            echo "$supplier->name \n";
+            echo( "Using company '{$supplier->name}'\n" );
         }
         
         echo( date('Y-m-d H:i:s').": CreateOrders run ended.\n\n" );
