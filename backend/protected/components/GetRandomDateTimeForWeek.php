6dfb6d61ee43858e34b203a7031fdbc9bd3e588a
Jarmo Kortetjärvi <jarmo.kortetjarvi@futurable.fi>
2013-09-26 13:39:38 +0300

    Add GetRandomDateTimeForWeek

---
+++ backend/protected/components/GetRandomDateTimeForWeek.php
@@ -0,0 +1,15 @@
+<?php
+Class GetRandomDateTimeForWeek{
+    public function run($week = false){
+        if( $week===false OR !in_array($week, range(1,52)) ) $week = date('W');
+        
+        $date = new DateTime();
+        // Set random day
+        $date->setISODate(date('Y'), $week, rand(0,7));
+        // Set random time from office hours
+        $date->setTime(rand(6,17), rand(0,60));
+        
+        return $date->format('c')."\n";
+    }
+}
+?>
