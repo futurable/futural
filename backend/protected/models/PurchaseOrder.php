f6600e2c679ebeffb27510e647396fe0940c4dfe
Jarmo Kortetjärvi <jarmo.kortetjarvi@futurable.fi>
2013-09-30 16:02:43 +0300

    Add invoice_method

--- backend/protected/models/PurchaseOrder.php
+++ backend/protected/models/PurchaseOrder.php
@@ -83,6 +83,7 @@ class PurchaseOrder extends CActiveRecord
             array('pricelist_id','default', 'value'=>'2', 'setOnEmpty'=>false,'on'=>'insert'),
             array('location_id','default', 'value'=>'12', 'setOnEmpty'=>false,'on'=>'insert'),
             array('state','default', 'value'=>'approved', 'setOnEmpty'=>false,'on'=>'insert'),
+            array('invoice_mehtod','default', 'value'=>'other', 'setOnEmpty'=>false,'on'=>'insert'),
         );
 	}
 
