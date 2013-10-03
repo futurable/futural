1965581b91555b285714dec30343cbb180016771
Jarmo Kortetjärvi <jarmo.kortetjarvi@futurable.fi>
2013-10-03 14:38:27 +0300

    Add orders fetching

--- backend/protected/commands/SendOrdersCommand.php
+++ backend/protected/commands/SendOrdersCommand.php
@@ -5,6 +5,18 @@ class SendOrdersCommand extends CConsoleCommand
     {   
         echo( date('Y-m-d H:i:s').": SendOrders run started.\n" );
         
+        # 1. See if there are orders to be sent
+        $criteria = new CDbCriteria();
+        $criteria->addCondition('sent IS NULL');
+        $orders = Order::model()->findAll( $criteria );
+        
+        if(empty($orders)){
+            die( "No sendable orders. Exiting.\n" );
+        }
+        else{
+            echo( count($orders)." unsent orders found\n" );
+        }
+            
         $transaction = Yii::app()->db->beginTransaction();
         $success = false;
         if($success){
