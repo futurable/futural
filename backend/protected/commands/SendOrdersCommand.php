e63af7d188ac871560aa935854ca7a65bbe4c257
Jarmo Kortetjärvi <jarmo.kortetjarvi@futurable.fi>
2013-10-03 14:35:31 +0300

    Fix conditions

--- backend/protected/commands/SendOrdersCommand.php
+++ backend/protected/commands/SendOrdersCommand.php
@@ -4,15 +4,18 @@ class SendOrdersCommand extends CConsoleCommand
     public function run($args)
     {   
         echo( date('Y-m-d H:i:s').": SendOrders run started.\n" );
-                 
-            if($IHSuccess AND $ILSuccess AND $POHSuccess AND $POLSuccess AND $orderSuccess){
-                echo( "Transaction successful\n" );
-                $transaction->commit();
-            }
-            else{
-                echo( "Transaction failed\n" );
-                $transaction->rollback();
-            }
+        
+        $transaction = Yii::app()->db->beginTransaction();
+        $success = false;
+        if($success){
+            echo( "Transaction successful\n" );
+            $transaction->commit();
+        }
+        else{
+            echo( "Transaction failed\n" );
+            $transaction->rollback();
+        }
+        
         echo( date('Y-m-d H:i:s').": SendOrders run ended.\n\n" );
     }
 }
