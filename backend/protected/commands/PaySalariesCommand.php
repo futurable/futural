4b97b0c035888bd11c908e386ef759fb108ef1e9
Jarmo Kortetjärvi <jarmo.kortetjarvi@futurable.fi>
2013-10-07 11:55:45 +0300

    Add PaySalariesCommand placeholder

---
+++ backend/protected/commands/PaySalariesCommand.php
@@ -0,0 +1,22 @@
+<?php
+class PaySalariesCommand extends CConsoleCommand
+{
+    public function run($args)
+    {   
+        echo( date('Y-m-d H:i:s').": PaySalaries run started.\n" );
+            
+        $transaction = Yii::app()->db->beginTransaction();
+
+        $success = false;
+        if($success){
+            echo( "Transaction successful\n" );
+            $transaction->commit();
+        }
+        else{
+            echo( "Transaction failed\n" );
+            $transaction->rollback();
+        }
+        
+        echo( date('Y-m-d H:i:s').": PaySalaries run ended.\n\n" );
+    }
+}
