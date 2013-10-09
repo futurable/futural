161652429c36726f52dc377d542f8c03f7a429bd
Jarmo Kortetjärvi <jarmo.kortetjarvi@futurable.fi>
2013-10-09 15:02:51 +0300

    Add bank accounts view

--- backend/protected/views/company/indexBankAccounts.php
+++ backend/protected/views/company/indexBankAccounts.php
@@ -1,3 +1,26 @@
 <?php
     echo "<h1>".Yii::t('Company', 'BankAccounts')."</h1>";
+    
+    echo "<table>";
+        echo "<tr>";
+            echo "<th>";
+                echo Yii::t('Company', 'Name');
+            echo "</th>"; 
+            echo "<th>";
+                echo Yii::t('BankAccount', 'iban');
+            echo "</th>"; 
+            echo "<th>";
+                echo Yii::t('BankAccount', 'saldo');
+            echo "</th>"; 
+        echo "</tr>";
+    
+    foreach($suppliers as $supplier){
+        echo "<tr>";
+            echo "<td>";
+            
+            echo "</td>"; 
+        echo "</tr>";
+    }
+    
+    echo "</table>";
 ?>
