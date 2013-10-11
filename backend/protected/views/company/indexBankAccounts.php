4745991b5e0a4179326c3582945910a38184b345
Jarmo Kortetjärvi <jarmo.kortetjarvi@futurable.fi>
2013-10-11 10:46:26 +0300

    Add some classes

--- backend/protected/views/company/indexBankAccounts.php
+++ backend/protected/views/company/indexBankAccounts.php
@@ -1,43 +1,48 @@
 <?php
     echo "<h1>".Yii::t('Company', 'BankAccounts')."</h1>";
     
-    echo "<table>";
-        echo "<tr>";
-            echo "<th>";
-                echo Yii::t('Company', 'Company');
-            echo "</th>"; 
-            echo "<th>";
-                echo Yii::t('BankAccount', 'Type');
-            echo "</th>"; 
-            echo "<th>";
-                echo Yii::t('BankAccount', 'iban');
-            echo "</th>"; 
-            echo "<th>";
-                echo Yii::t('BankAccount', 'Saldo');
-            echo "</th>"; 
-        echo "</tr>";
+    echo "<div class=grid-view>";
     
-    foreach($suppliers as $key => $supplier){
-        $bankUser = $bankUsers[$key];
-        
-        echo "<tr>";
-            echo "<td>";
-                echo $bankUser->bankProfile->company;
-            echo "</td>"; 
-        echo "</tr>";
-        
-        foreach($bankUser->bankAccounts as $bankAccount){
+        echo "<table class='items table table-striped'>";
+            echo "<thead>";
+                echo "<tr>";
+                    echo "<th>";
+                        echo Yii::t('Company', 'Company');
+                    echo "</th>"; 
+                    echo "<th>";
+                        echo Yii::t('BankAccount', 'Type');
+                    echo "</th>"; 
+                    echo "<th>";
+                        echo Yii::t('BankAccount', 'iban');
+                    echo "</th>"; 
+                    echo "<th>";
+                        echo Yii::t('BankAccount', 'Saldo');
+                    echo "</th>"; 
+                echo "</tr>";
+            echo "</thead>";
+
+        foreach($suppliers as $key => $supplier){
+            $bankUser = $bankUsers[$key];
+
             echo "<tr>";
-                echo "<td/>";
                 echo "<td>";
-                    echo $bankAccount->bankAccountType->description;
-                echo "</td>"; 
-                echo "<td>";
-                    echo $bankAccount->iban;
+                    echo $bankUser->bankProfile->company;
                 echo "</td>"; 
             echo "</tr>";
+
+            foreach($bankUser->bankAccounts as $bankAccount){
+                echo "<tr>";
+                    echo "<td/>";
+                    echo "<td>";
+                        echo $bankAccount->bankAccountType->description;
+                    echo "</td>"; 
+                    echo "<td>";
+                        echo $bankAccount->iban;
+                    echo "</td>"; 
+                echo "</tr>";
+            }
         }
-    }
-    
-    echo "</table>";
+        echo "</table>";
+        
+    echo "</div>";
 ?>
