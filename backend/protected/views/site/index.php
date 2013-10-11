9b1cd1de2b8327b0954d67742ebe26a87f66670d
Jarmo Kortetjärvi <jarmo.kortetjarvi@futurable.fi>
2013-10-11 10:59:09 +0300

    Modify formatting

--- backend/protected/views/site/index.php
+++ backend/protected/views/site/index.php
@@ -4,13 +4,17 @@
 $this->pageTitle=Yii::app()->name;
 ?>
 
-<?php if(!Yii::app()->user->isGuest){ ?>
+<?php 
+
+    if(Yii::app()->user->isGuest){
+        $this->redirect(array('/site/login'));
+    }
+?>
 
     <h1><?php echo CHtml::encode(Yii::app()->name) ;?></h1>
 
-    <p><?php echo Yii::t('Index', 'YouAreLoggedInAs')."<br/><strong>".
-            Yii::app()->user->tokenCustomer->name; ?></strong></p>
-<?php
-}
-else $this->redirect(array('/site/login'));
-?>
+    <p>
+        <?php echo Yii::t('Index', 'YouAreLoggedInAs')."<br/>
+            <strong>".Yii::app()->user->tokenCustomer->name."</strong>";
+         ?>
+    </p>
