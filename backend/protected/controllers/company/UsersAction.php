0ce8846cb33456dc91537bd572386bd4fab3f451
Jarmo Kortetjärvi <jarmo.kortetjarvi@futurable.fi>
2013-10-16 18:36:24 +0300

    Move the controller actions to the controller

--- backend/protected/controllers/company/UsersAction.php
+++ backend/protected/controllers/company/UsersAction.php
@@ -6,11 +6,29 @@ class UsersAction extends CAction
         $controller=$this->getController();
         $controller->allowUser(MANAGER);
         
-        // Get all suppliers
+        // Get all companies
         $companies = Suppliers::get();
+
+        $companyArray = array();
+        foreach($companies AS $company){
+            // Switch to the right company
+            Yii::app()->dbopenerp->setActive(false);
+            Yii::app()->dbopenerp->connectionString = "pgsql:host=erp.futurality.fi;dbname={$company['tag']}";
+            Yii::app()->dbopenerp->setActive(true);
+
+            // Get the OpenERP-company
+            $OECompany = ResCompany::model()->find();
+            
+            // Get company users
+            $OEEmployees = HrEmployee::model()->findAll();
+            
+            $companyArray[$company->name]['company'] = $company;
+            $companyArray[$company->name]['OECompany'] = $OECompany;
+            $companyArray[$company->name]['OEEmployees'] = $OEEmployees;
+        }
         
         $controller->render('users',array(
-            'companies'=>$companies,
+            'companies'=>$companyArray,
         ));
     }
 }
