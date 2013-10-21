fe040f8375bd17034b51beea7f55a944529921cc
Jarmo Kortetjärvi <jarmo.kortetjarvi@futurable.fi>
2013-10-21 16:39:58 +0300

    Fix the set_time ordering

--- backend/protected/components/GetRandomDateTimeForWeek.php
+++ backend/protected/components/GetRandomDateTimeForWeek.php
@@ -4,10 +4,10 @@ Class GetRandomDateTimeForWeek{
         if( $week===false OR !in_array($week, range(1,52)) ) $week = date('W');
         
         $date = new DateTime();
-        // Set random day
-        $date->setISODate(date('Y'), $week, rand(0,7));
-        // Set random time from office hours
+        // Set a random time from office hours
         $date->setTime(rand(6,17), rand(0,60));
+        // Set a random day
+        $date->setISODate(date('Y'), $week, rand(0,7));
         
         return $date->format('Y-m-d H:i:s');
     }
