453b2b85d646436d79f14b26389e73d43bf77343
Jarmo Kortetjärvi <jarmo.kortetjarvi@futurable.fi>
2013-10-22 13:56:21 +0300

    Add event date

--- backend/protected/views/company/_createRemark.php
+++ backend/protected/views/company/_createRemark.php
@@ -10,18 +10,32 @@
 	<table>
 		<tr>
             <th>
-                <?php echo $form->labelEx($remark,'description'); ?>
-                <?php echo $form->error($remark,'description'); ?>
+                <?php echo $form->label($remark,'description'); ?>
             </th>
             <th>
-                <?php echo $form->labelEx($remark,'significance'); ?>
-                <?php echo $form->error($remark,'significance'); ?>
+                <?php echo $form->label($remark,'significance'); ?>
+            </th>
+            <th>
+                <?php echo $form->label($remark,'event_date'); ?>
             </th>
         </tr>
 
         <tr>
             <td><?php echo $form->textField($remark,'description',array('size'=>60,'maxlength'=>1024)); ?></td>
-            <td><?php echo $form->dropDownList($remark,'significance', range(-5,5), array('size'=>1)); ?></td>
+            <td><?php echo $form->dropDownList($remark,'significance', $remark->getSignificanceArray(), array('options'=>array('0'=>array('selected'=>true)))); ?></td>
+            <td>
+                <?php 
+                $this->widget('zii.widgets.jui.CJuiDatePicker', array(
+                    'model'=>$remark,
+                    'attribute'=>'event_date',
+                    'language'=>Yii::app()->language,
+                    // additional javascript options for the date picker plugin
+                    'options'=>array(
+                        'defaultDate'=>$remark->event_date,
+                    ),
+                ));
+                ?>
+            </td>
             <td><?php $this->widget('bootstrap.widgets.TbButton', array('buttonType'=>'submit', 'type'=>'info', 'label'=>Yii::t('Company', 'Create'))); ?></td>
         </tr>	
 	</table>
