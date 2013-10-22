b7da23bdf1085089761120f7a87b01dd94570bc4
Jarmo Kortetjärvi <jarmo.kortetjarvi@futurable.fi>
2013-10-22 13:10:29 +0300

    Add remark printing

--- backend/protected/views/company/_viewRemarks.php
+++ backend/protected/views/company/_viewRemarks.php
@@ -1,3 +1,23 @@
 <?php
 echo "<h2>".Yii::t('Company', 'Remarks')."</h2>";
+
+if(!empty($remarks)){
+    echo "<table>";
+        echo "<tr>";
+            echo "<th>".Yii::t('Company', 'CreateDate')."</th>";
+            echo "<th>".Yii::t('Company', 'Remark')."</th>";
+            echo "<th>".Yii::t('Company', 'Significance')."</th>";
+        echo "</tr>";
+    foreach($remarks as $remark){
+        echo "<tr>";
+            echo "<td>{$remark->create_date}</td>";
+            echo "<td>{$remark->description}</td>";
+            echo "<td>{$remark->sigificance}</td>";
+        echo "</tr>";
+    }
+    echo "</table>";
+}
+else{
+    echo "<p>".Yii::t('Company','NoRemarks')."</p>";
+}
 ?>
