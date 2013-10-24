61e4a71737c08e4fc63e38def2f8115814228533
jarmo <jarmo@futurable-jarmo>
2013-10-24 15:42:00 +0300

    Rework the whole view

--- backend/protected/views/company/_viewOpenErpPurchaseOrders.php
+++ backend/protected/views/company/_viewOpenErpPurchaseOrders.php
@@ -1,31 +1,26 @@
 <?php
    echo "<h2>".Yii::t('Company', 'PurchaseOrders')."</h2>";
     
-    $gridDataProvider = new CArrayDataProvider($OEPurchaseOrders, array(           
-        'pagination'=>array(
-            'pageSize' => 5,
-         ),
-    ));
-
-    $gridColumns = array(
-        array('name'=>'create_date', 'header' => Yii::t('Company', 'CreateDate')),
-        array('name'=>'amount_total', 'header' => Yii::t('Company', 'TotalAmount')),
-        array('name'=>'state', 'header' => Yii::t('Company', 'OrderState')),
-        array(
-            'class'=>'bootstrap.widgets.TbButtonColumn',
-            'viewButtonUrl'=>null,
-            'buttons'=>array(
-                'view'=>array('visible'=>'false',),
-                'update'=>array('visible'=>'false',),
-                'delete'=>array('visible'=>'false',),
-            ),
-        )
-    );
-
-    $this->widget('bootstrap.widgets.TbGridView', array(
-        'type'=>'striped',
-        'dataProvider'=>$gridDataProvider,
-        'template'=>"{items}{pager}",
-        'columns'=>$gridColumns,
-    ));
+   echo "<table class='table-striped table-condensed'>";
+   echo "<tr>";
+        echo "<th>".Yii::t('Order', 'Creator')."</th>";
+        echo "<th>".Yii::t('Order', 'CreateDate')."</th>";
+        echo "<th>".Yii::t('Order', 'AmountUntaxed')."</th>";
+        echo "<th>".Yii::t('Order', 'AmountTax')."</th>";
+        echo "<th>".Yii::t('Order', 'TotalAmount')."</th>";
+        echo "<th>".Yii::t('Order', 'OrderState')."</th>";
+   echo "</tr>";
+   foreach($OEPurchaseOrders as $PurchaseOrder){
+       $createDate = date('d.m.Y', strtotime($PurchaseOrder->create_date));
+       
+       echo "<tr>";
+            echo "<td>{$PurchaseOrder->createU->partner->name}</td>";
+            echo "<td>{$createDate}</td>";
+            echo "<td>{$PurchaseOrder->amount_untaxed}&euro;</td>";
+            echo "<td>{$PurchaseOrder->amount_tax}</td>";
+            echo "<td>{$PurchaseOrder->amount_total}</td>";
+            echo "<td>{$PurchaseOrder->state}</td>";
+       echo "</tr>";
+   }
+   echo "</table>";
 ?>
