7b60dd9082f67cd38542d9a93c1682edaabf265a
Jarmo Kortetjärvi <jarmo.kortetjarvi@futurable.fi>
2013-11-19 11:35:56 +0200

    Add attributes

--- backend/protected/views/company/_viewOpenErpEmployees.php
+++ backend/protected/views/company/_viewOpenErpEmployees.php
@@ -1,7 +1,7 @@
 <?php
    echo "<h2>".Yii::t('Company', 'Employees')."</h2>";
     
-    echo "<div class='grid-view'>";
+    echo "<div class='grid-view' id='HrEmployees'>";
     
         echo "<table class='items table table-striped'>";
             echo "<thead>";
@@ -11,6 +11,8 @@
                      echo "<th>".Yii::t('Company', 'Email')."</th>";
                      echo "<th>".Yii::t('Company', 'PurchaseOrders')."</th>";
                      echo "<th>".Yii::t('Company', 'SaleOrders')."</th>";
+                     echo "<th>".Yii::t('Company', 'BookingEntries')."</th>";
+                     echo "<th>".Yii::t('Company', 'Products')."</th>";
                 echo "</tr>";
             echo "</thead>";
             echo "<tbody>";
@@ -22,6 +24,8 @@
                          echo "<td>{$OEEmployee->work_email}</td>";
                          echo "<td>{$OEEmployee->purchaseOrdersCreated}</td>";
                          echo "<td>{$OEEmployee->saleOrdersCreated}</td>";
+                         echo "<td>{$OEEmployee->accountMoveLinesCreated}</td>";
+                         echo "<td>{$OEEmployee->productsCreated}</td>";
                     echo "</tr>";
                 }
             echo "</tbody>";
