7a57cebd08569908c21f9377d9de921f38cdc278
Jarmo Kortetjärvi <jarmo.kortetjarvi@futurable.fi>
2013-11-21 13:30:43 +0200

    Add menu elements

--- backend/protected/views/company/view.php
+++ backend/protected/views/company/view.php
@@ -8,24 +8,30 @@
             $subMenu[] = array('label'=>$menuItem->name, 'url'=>array("/company/view", 'id' => $menuItem->id, 'action'=>$action));
         }
 
-        echo "<div id='submenu'>";
+        echo "<div id='companymenu'>";
             $this->widget('zii.widgets.CMenu',array(
                 'items'=>$subMenu,
             ));
         echo "</div>";
     }
 
-    $this->menu=array(
-        array('label'=>Yii::t('Company', 'CompanyInfo'), 'url'=>array("/company/view", 'id' => $company->id, 'action'=>'info')),
-        array('label'=>Yii::t('Company', 'CostBenefitCalculation'), 'url'=>array("/company/view", 'id' => $company->id, 'action'=>'costBenefitCalculation')),
-        array('label'=>Yii::t('Company', 'BankAccounts'), 'url'=>array("/company/view", 'id' => $company->id, 'action'=>'bankAccounts')),
-        array('label'=>Yii::t('Company', 'CustomerPayments'), 'url'=>array("/company/view", 'id' => $company->id, 'action'=>'CustomerPayments'), 'visible'=>$role>0),
-        array('label'=>Yii::t('Company', 'Employees'), 'url'=>array("/company/view", 'id' => $company->id, 'action'=>'employees')),
-        array('label'=>Yii::t('Company', 'SaleOrders'), 'url'=>array("/company/view", 'id' => $company->id, 'action'=>'saleOrders')),
-        array('label'=>Yii::t('Company', 'PurchaseOrders'), 'url'=>array("/company/view", 'id' => $company->id, 'action'=>'purchaseOrders')),
-        array('label'=>Yii::t('Company', 'AutomatedOrders'), 'url'=>array("/company/view", 'id' => $company->id, 'action'=>'automatedOrders'), 'visible'=>$role>0),
-        array('label'=>Yii::t('Company', 'Remarks'), 'url'=>array("/company/view", 'id' => $company->id, 'action'=>'remarks')),
-    );
+    echo "<div id='submenu'>";
+        $this->widget('zii.widgets.CMenu', array(
+            'items' => array(
+                array('label'=>Yii::t('Company', 'CompanyInfo'), 'url'=>array("/company/view", 'id' => $company->id, 'action'=>'info')),
+                array('label'=>Yii::t('Company', 'CostBenefitCalculation'), 'url'=>array("/company/view", 'id' => $company->id, 'action'=>'costBenefitCalculation')),
+                array('label'=>Yii::t('Company', 'BankAccounts'), 'url'=>array("/company/view", 'id' => $company->id, 'action'=>'bankAccounts')),
+                array('label'=>Yii::t('Company', 'CustomerPayments'), 'url'=>array("/company/view", 'id' => $company->id, 'action'=>'CustomerPayments'), 'visible'=>$role>0),
+                array('label'=>Yii::t('Company', 'Employees'), 'url'=>array("/company/view", 'id' => $company->id, 'action'=>'employees')),
+                array('label'=>Yii::t('Company', 'Timesheets'), 'url'=>array("/company/view", 'id' => $company->id, 'action'=>'timesheets')),
+                array('label'=>Yii::t('Company', 'Attendance'), 'url'=>array("/company/view", 'id' => $company->id, 'action'=>'attendance')),
+                array('label'=>Yii::t('Company', 'SaleOrders'), 'url'=>array("/company/view", 'id' => $company->id, 'action'=>'saleOrders')),
+                array('label'=>Yii::t('Company', 'PurchaseOrders'), 'url'=>array("/company/view", 'id' => $company->id, 'action'=>'purchaseOrders')),
+                array('label'=>Yii::t('Company', 'AutomatedOrders'), 'url'=>array("/company/view", 'id' => $company->id, 'action'=>'automatedOrders'), 'visible'=>$role>0),
+                array('label'=>Yii::t('Company', 'Remarks'), 'url'=>array("/company/view", 'id' => $company->id, 'action'=>'remarks')),
+            )
+        ));
+     echo "</div>";
 
     echo "<h1>{$company->name}</h1>";
     
